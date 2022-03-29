909779092d4676e23d85cb3d1c7cd90f5fd83582
nimakarimipour <karimipour.nima@gmail.com>
2022-03-29 02:36:16 -0700

    Final Result

--- jadx-core/src/main/java/jadx/core/dex/instructions/BaseInvokeNode.java
+++ jadx-core/src/main/java/jadx/core/dex/instructions/BaseInvokeNode.java
@@ -1,25 +1,24 @@
 package jadx.core.dex.instructions;
 
 import org.jetbrains.annotations.Nullable;
-
 import jadx.core.dex.info.MethodInfo;
 import jadx.core.dex.instructions.args.InsnArg;
 import jadx.core.dex.nodes.InsnNode;
 
 public abstract class BaseInvokeNode extends InsnNode {
-	public BaseInvokeNode(InsnType type, int argsCount) {
-		super(type, argsCount);
-	}
 
-	public abstract MethodInfo getCallMth();
+    public BaseInvokeNode(InsnType type, int argsCount) {
+        super(type, argsCount);
+    }
 
-	
-	public abstract InsnArg getInstanceArg();
+    public abstract MethodInfo getCallMth();
 
-	public abstract boolean isStaticCall();
+    public abstract InsnArg getInstanceArg();
 
-	/**
-	 * Return offset to match method args from {@link #getCallMth()}
-	 */
-	public abstract int getFirstArgOffset();
+    public abstract boolean isStaticCall();
+
+    /**
+     * Return offset to match method args from {@link #getCallMth()}
+     */
+    public abstract int getFirstArgOffset();
 }
