a9f0af6f25dc0ad51410f36490663b593bba5636
nimakarimipour <karimipour.nima@gmail.com>
2022-03-29 02:02:14 -0700

    Final Result

--- jadx-core/src/main/java/jadx/core/dex/visitors/AttachMethodDetails.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/AttachMethodDetails.java
@@ -1,5 +1,6 @@
 package jadx.core.dex.visitors;
 
+import jadx.Initializer;
 import jadx.core.dex.instructions.BaseInvokeNode;
 import jadx.core.dex.nodes.BlockNode;
 import jadx.core.dex.nodes.IMethodDetails;
@@ -11,44 +12,35 @@ import jadx.core.dex.visitors.shrink.CodeShrinkVisitor;
 import jadx.core.dex.visitors.typeinference.TypeInferenceVisitor;
 import jadx.core.utils.exceptions.JadxException;
 
-@JadxVisitor(
-		name = "Attach Method Details",
-		desc = "Attach method details for invoke instructions",
-		runBefore = {
-				CodeShrinkVisitor.class,
-				TypeInferenceVisitor.class,
-				MethodInvokeVisitor.class,
-				OverrideMethodVisitor.class
-		}
-)
+@JadxVisitor(name = "Attach Method Details", desc = "Attach method details for invoke instructions", runBefore = { CodeShrinkVisitor.class, TypeInferenceVisitor.class, MethodInvokeVisitor.class, OverrideMethodVisitor.class })
 public class AttachMethodDetails extends AbstractVisitor {
 
-	private MethodUtils methodUtils;
+    private MethodUtils methodUtils;
 
-	@Override
-	public void init(RootNode root) {
-		methodUtils = root.getMethodUtils();
-	}
+    @Override
+    @Initializer
+    public void init(RootNode root) {
+        methodUtils = root.getMethodUtils();
+    }
 
-	@Override
-	public void visit(MethodNode mth) throws JadxException {
-		if (mth.isNoCode()) {
-			return;
-		}
-		for (BlockNode blockNode : mth.getBasicBlocks()) {
-			for (InsnNode insn : blockNode.getInstructions()) {
-				if (insn instanceof BaseInvokeNode) {
-					attachMethodDetails((BaseInvokeNode) insn);
-				}
-			}
-		}
-	}
+    @Override
+    public void visit(MethodNode mth) throws JadxException {
+        if (mth.isNoCode()) {
+            return;
+        }
+        for (BlockNode blockNode : mth.getBasicBlocks()) {
+            for (InsnNode insn : blockNode.getInstructions()) {
+                if (insn instanceof BaseInvokeNode) {
+                    attachMethodDetails((BaseInvokeNode) insn);
+                }
+            }
+        }
+    }
 
-	private void attachMethodDetails(BaseInvokeNode insn) {
-		IMethodDetails methodDetails = methodUtils.getMethodDetails(insn.getCallMth());
-		if (methodDetails != null) {
-			insn.addAttr(methodDetails);
-		}
-	}
-
+    private void attachMethodDetails(BaseInvokeNode insn) {
+        IMethodDetails methodDetails = methodUtils.getMethodDetails(insn.getCallMth());
+        if (methodDetails != null) {
+            insn.addAttr(methodDetails);
+        }
+    }
 }
