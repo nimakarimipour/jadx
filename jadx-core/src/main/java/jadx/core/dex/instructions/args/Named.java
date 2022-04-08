c93c421643b50e5066acdbb8826611ecd32dd79d
nimakarimipour <karimipour.nima@gmail.com>
2022-04-07 20:15:51 -0700

    Final Result

--- jadx-core/src/main/java/jadx/core/dex/instructions/args/Named.java
+++ jadx-core/src/main/java/jadx/core/dex/instructions/args/Named.java
@@ -1,8 +1,11 @@
 package jadx.core.dex.instructions.args;
 
+import org.jetbrains.annotations.Nullable;
+
 public interface Named {
 
-	String getName();
+    @Nullable
+    String getName();
 
-	void setName(String name);
+    void setName(String name);
 }
