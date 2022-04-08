77a85ea904de0353593f00b552a9a04a9d9df302
nimakarimipour <karimipour.nima@gmail.com>
2022-04-07 18:54:45 -0700

    Final Result

--- jadx-core/src/main/java/jadx/core/dex/visitors/regions/variables/VarUsage.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/regions/variables/VarUsage.java
@@ -1,33 +1,38 @@
 package jadx.core.dex.visitors.regions.variables;
 
+import org.jetbrains.annotations.Nullable;
 import java.util.ArrayList;
 import java.util.List;
-
 import jadx.core.dex.instructions.args.SSAVar;
 
 class VarUsage {
-	private final SSAVar var;
-	private final List<UsePlace> assigns = new ArrayList<>(3);
-	private final List<UsePlace> uses = new ArrayList<>(3);
 
-	VarUsage(SSAVar var) {
-		this.var = var;
-	}
+    @Nullable
+    private final SSAVar var;
 
-	public SSAVar getVar() {
-		return var;
-	}
+    private final List<UsePlace> assigns = new ArrayList<>(3);
 
-	public List<UsePlace> getAssigns() {
-		return assigns;
-	}
+    private final List<UsePlace> uses = new ArrayList<>(3);
 
-	public List<UsePlace> getUses() {
-		return uses;
-	}
+    VarUsage(@Nullable SSAVar var) {
+        this.var = var;
+    }
 
-	@Override
-	public String toString() {
-		return '{' + (var == null ? "-" : var.toShortString()) + ", a:" + assigns + ", u:" + uses + '}';
-	}
+    @Nullable
+    public SSAVar getVar() {
+        return var;
+    }
+
+    public List<UsePlace> getAssigns() {
+        return assigns;
+    }
+
+    public List<UsePlace> getUses() {
+        return uses;
+    }
+
+    @Override
+    public String toString() {
+        return '{' + (var == null ? "-" : var.toShortString()) + ", a:" + assigns + ", u:" + uses + '}';
+    }
 }
