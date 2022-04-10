0d60fe7bfc9dd0448f47ba04ea0d7ec4439d6065
nimakarimipour <karimipour.nima@gmail.com>
2022-04-10 02:21:21 -0700

    Final Result

--- jadx-core/src/main/java/jadx/core/codegen/json/mapping/JsonMapping.java
+++ jadx-core/src/main/java/jadx/core/codegen/json/mapping/JsonMapping.java
@@ -1,15 +1,19 @@
 package jadx.core.codegen.json.mapping;
 
+import org.jetbrains.annotations.Nullable;
 import java.util.List;
 
 public class JsonMapping {
-	private List<JsonClsMapping> classes;
 
-	public List<JsonClsMapping> getClasses() {
-		return classes;
-	}
+    @Nullable
+    private List<JsonClsMapping> classes;
 
-	public void setClasses(List<JsonClsMapping> classes) {
-		this.classes = classes;
-	}
+    @Nullable
+    public List<JsonClsMapping> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(List<JsonClsMapping> classes) {
+        this.classes = classes;
+    }
 }
