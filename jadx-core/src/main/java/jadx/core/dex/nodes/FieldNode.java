8604e29f642487f7ba12a9adfec5c45d5d545f1f
nimakarimipour <karimipour.nima@gmail.com>
2022-04-14 18:47:51 -0700

    Final Result

--- jadx-core/src/main/java/jadx/core/dex/nodes/FieldNode.java
+++ jadx-core/src/main/java/jadx/core/dex/nodes/FieldNode.java
@@ -1,8 +1,8 @@
 package jadx.core.dex.nodes;
 
+import org.jetbrains.annotations.Nullable;
 import java.util.Collections;
 import java.util.List;
-
 import jadx.api.plugins.input.data.IFieldData;
 import jadx.core.dex.attributes.annotations.AnnotationsList;
 import jadx.core.dex.attributes.nodes.LineAttrNode;
@@ -13,108 +13,110 @@ import jadx.core.dex.instructions.args.ArgType;
 
 public class FieldNode extends LineAttrNode implements ICodeNode {
 
-	private final ClassNode parentClass;
-	private final FieldInfo fieldInfo;
-	private AccessInfo accFlags;
+    private final ClassNode parentClass;
 
-	private ArgType type;
+    private final FieldInfo fieldInfo;
 
-	private List<MethodNode> useIn = Collections.emptyList();
+    private AccessInfo accFlags;
 
-	public static FieldNode build(ClassNode cls, IFieldData fieldData) {
-		FieldInfo fieldInfo = FieldInfo.fromData(cls.root(), fieldData);
-		FieldNode fieldNode = new FieldNode(cls, fieldInfo, fieldData.getAccessFlags());
-		AnnotationsList.attach(fieldNode, fieldData.getAnnotations());
-		return fieldNode;
-	}
+    private ArgType type;
 
-	public FieldNode(ClassNode cls, FieldInfo fieldInfo, int accessFlags) {
-		this.parentClass = cls;
-		this.fieldInfo = fieldInfo;
-		this.type = fieldInfo.getType();
-		this.accFlags = new AccessInfo(accessFlags, AFType.FIELD);
-	}
+    private List<MethodNode> useIn = Collections.emptyList();
 
-	public void updateType(ArgType type) {
-		this.type = type;
-	}
+    public static FieldNode build(ClassNode cls, IFieldData fieldData) {
+        FieldInfo fieldInfo = FieldInfo.fromData(cls.root(), fieldData);
+        FieldNode fieldNode = new FieldNode(cls, fieldInfo, fieldData.getAccessFlags());
+        AnnotationsList.attach(fieldNode, fieldData.getAnnotations());
+        return fieldNode;
+    }
 
-	public FieldInfo getFieldInfo() {
-		return fieldInfo;
-	}
+    public FieldNode(ClassNode cls, FieldInfo fieldInfo, int accessFlags) {
+        this.parentClass = cls;
+        this.fieldInfo = fieldInfo;
+        this.type = fieldInfo.getType();
+        this.accFlags = new AccessInfo(accessFlags, AFType.FIELD);
+    }
 
-	@Override
-	public AccessInfo getAccessFlags() {
-		return accFlags;
-	}
+    public void updateType(ArgType type) {
+        this.type = type;
+    }
 
-	@Override
-	public void setAccessFlags(AccessInfo accFlags) {
-		this.accFlags = accFlags;
-	}
+    public FieldInfo getFieldInfo() {
+        return fieldInfo;
+    }
 
-	public boolean isStatic() {
-		return accFlags.isStatic();
-	}
+    @Override
+    public AccessInfo getAccessFlags() {
+        return accFlags;
+    }
 
-	public String getName() {
-		return fieldInfo.getName();
-	}
+    @Override
+    public void setAccessFlags(AccessInfo accFlags) {
+        this.accFlags = accFlags;
+    }
 
-	public String getAlias() {
-		return fieldInfo.getAlias();
-	}
+    public boolean isStatic() {
+        return accFlags.isStatic();
+    }
 
-	public ArgType getType() {
-		return type;
-	}
+    public String getName() {
+        return fieldInfo.getName();
+    }
 
-	public ClassNode getParentClass() {
-		return parentClass;
-	}
+    public String getAlias() {
+        return fieldInfo.getAlias();
+    }
 
-	public List<MethodNode> getUseIn() {
-		return useIn;
-	}
+    public ArgType getType() {
+        return type;
+    }
 
-	public void setUseIn(List<MethodNode> useIn) {
-		this.useIn = useIn;
-	}
+    public ClassNode getParentClass() {
+        return parentClass;
+    }
 
-	@Override
-	public String typeName() {
-		return "field";
-	}
+    public List<MethodNode> getUseIn() {
+        return useIn;
+    }
 
-	@Override
-	public String getInputFileName() {
-		return parentClass.getInputFileName();
-	}
+    public void setUseIn(List<MethodNode> useIn) {
+        this.useIn = useIn;
+    }
 
-	@Override
-	public RootNode root() {
-		return parentClass.root();
-	}
+    @Override
+    public String typeName() {
+        return "field";
+    }
 
-	@Override
-	public int hashCode() {
-		return fieldInfo.hashCode();
-	}
+    @Override
+    public String getInputFileName() {
+        return parentClass.getInputFileName();
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null || getClass() != obj.getClass()) {
-			return false;
-		}
-		FieldNode other = (FieldNode) obj;
-		return fieldInfo.equals(other.fieldInfo);
-	}
+    @Override
+    public RootNode root() {
+        return parentClass.root();
+    }
 
-	@Override
-	public String toString() {
-		return fieldInfo.getDeclClass() + "." + fieldInfo.getName() + " :" + type;
-	}
+    @Override
+    public int hashCode() {
+        return fieldInfo.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        FieldNode other = (FieldNode) obj;
+        return fieldInfo.equals(other.fieldInfo);
+    }
+
+    @Override
+    public String toString() {
+        return fieldInfo.getDeclClass() + "." + fieldInfo.getName() + " :" + type;
+    }
 }
