9aa117fb47bd0bc1159571154a0702a1d77a56ae
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 20:35:14 -0700

    Revert "fix: 62 - RETURN_NULLABLE - return null; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 6dc9a7834097ec4a5f30271bfe3c94cfcc7e19c2.

--- jadx-core/src/main/java/jadx/core/dex/instructions/args/ArgType.java
+++ jadx-core/src/main/java/jadx/core/dex/instructions/args/ArgType.java
@@ -600,7 +600,7 @@ public abstract class ArgType {
 	}
 
 	public List<ArgType> getGenericTypes() {
-		return Collections.emptyList();
+		return null;
 	}
 
 	public List<ArgType> getExtendTypes() {
