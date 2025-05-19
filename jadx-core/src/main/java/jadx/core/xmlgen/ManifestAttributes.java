a60338726ec47d4f0202f0e7274c71d80ebbe874
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 20:33:45 -0700

    Revert "fix: 43 - RETURN_NULLABLE - return instance; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 51a074e88ed6c7c66be0b2a74d0f5282846e8566.

--- jadx-core/src/main/java/jadx/core/xmlgen/ManifestAttributes.java
+++ jadx-core/src/main/java/jadx/core/xmlgen/ManifestAttributes.java
@@ -62,7 +62,6 @@ public class ManifestAttributes {
 				instance = new ManifestAttributes();
 			} catch (Exception e) {
 				LOG.error("Failed to create ManifestAttributes", e);
-				instance = new ManifestAttributes(); // Create a new instance to ensure non-null return
 			}
 		}
 		return instance;
