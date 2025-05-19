c710cf6fe519e1a16fb0c91a8ae12c0ef66aeb2b
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 20:31:56 -0700

    fix: 20 - RETURN_NULLABLE - return null; - returning @Nullable expression from method with @NonNull return type

--- jadx-core/src/main/java/jadx/core/dex/nodes/parser/SignatureParser.java
+++ jadx-core/src/main/java/jadx/core/dex/nodes/parser/SignatureParser.java
@@ -156,7 +156,7 @@ public class SignatureParser {
 				return ArgType.array(consumeType());
 
 			case STOP_CHAR:
-				return null;
+				throw new JadxRuntimeException("Unexpected STOP_CHAR reached in type parsing.");
 
 			default:
 				// primitive type (one char)
