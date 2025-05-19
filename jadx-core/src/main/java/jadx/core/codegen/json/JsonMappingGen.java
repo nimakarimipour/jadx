3c389c6cc7c1bc8970e838d8772c24f969e503a5
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 20:35:46 -0700

    fix: 68 - DEREFERENCE_NULLABLE - mapping.getClasses().add(jsonCls); - dereferenced expression mapping.getClasses() is @Nullable

--- jadx-core/src/main/java/jadx/core/codegen/json/JsonMappingGen.java
+++ jadx-core/src/main/java/jadx/core/codegen/json/JsonMappingGen.java
@@ -54,7 +54,8 @@ public class JsonMappingGen {
 
 	private static void fillMapping(JsonMapping mapping, RootNode root) {
 		List<ClassNode> classes = root.getClasses(true);
-		mapping.setClasses(new ArrayList<>(classes.size()));
+		List<JsonClsMapping> jsonClsMappings = new ArrayList<>(classes.size());
+		mapping.setClasses(jsonClsMappings);
 		for (ClassNode cls : classes) {
 			ClassInfo classInfo = cls.getClassInfo();
 			JsonClsMapping jsonCls = new JsonClsMapping();
@@ -67,7 +68,7 @@ public class JsonMappingGen {
 			}
 			addFields(cls, jsonCls);
 			addMethods(cls, jsonCls);
-			mapping.getClasses().add(jsonCls);
+			jsonClsMappings.add(jsonCls);
 		}
 	}
 
