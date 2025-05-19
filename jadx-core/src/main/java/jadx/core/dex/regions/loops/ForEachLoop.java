b51803c055e95df6018743f00fd2158a9117c9be
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 20:31:03 -0700

    fix: 10 - DEREFERENCE_NULLABLE - loopRegion.getHeader().getInstructions().add(0, varArgInsn); - dereferenced expression loopRegion.getHeader() is @Nullable

--- jadx-core/src/main/java/jadx/core/dex/regions/loops/ForEachLoop.java
+++ jadx-core/src/main/java/jadx/core/dex/regions/loops/ForEachLoop.java
@@ -26,8 +26,12 @@ public final class ForEachLoop extends LoopType {
 	}
 
 	public void injectFakeInsns(LoopRegion loopRegion) {
-		loopRegion.getInfo().getPreHeader().getInstructions().add(iterableArgInsn);
-		loopRegion.getHeader().getInstructions().add(0, varArgInsn);
+		if (loopRegion.getInfo().getPreHeader() != null) {
+			loopRegion.getInfo().getPreHeader().getInstructions().add(iterableArgInsn);
+		}
+		if (loopRegion.getHeader() != null) {
+			loopRegion.getHeader().getInstructions().add(0, varArgInsn);
+		}
 	}
 
 	public RegisterArg getVarArg() {
