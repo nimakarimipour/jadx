d49c2866b935ddaa397c226f3274af298c75a1f3
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 20:33:53 -0700

    fix: 45 - RETURN_NULLABLE - return region; - returning @Nullable expression from method with @NonNull return type

--- jadx-core/src/main/java/jadx/core/dex/nodes/MethodNode.java
+++ jadx-core/src/main/java/jadx/core/dex/nodes/MethodNode.java
@@ -536,6 +536,9 @@ public class MethodNode extends NotificationAttrNode implements IMethodDetails, 
 	}
 
 	public Region getRegion() {
+		if (region == null) {
+			region = new Region(); // Or however Region should be instantiated
+		}
 		return region;
 	}
 
