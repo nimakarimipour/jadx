2ccc1f2c3ad76853a135b87e89c425e1fd82d743
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 20:37:57 -0700

    Revert "fix: 93 - METHOD_NO_INIT - public FillArrayInsn(InsnArg arg, int target) { - initializer method does not guarantee @NonNull field arrayData (line 13) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit e980a5324e9c3fefc51a123c177b74f3f399a7a6.

--- jadx-core/src/main/java/jadx/core/dex/instructions/FillArrayInsn.java
+++ jadx-core/src/main/java/jadx/core/dex/instructions/FillArrayInsn.java
@@ -15,7 +15,6 @@ public final class FillArrayInsn extends InsnNode {
 	public FillArrayInsn(InsnArg arg, int target) {
 		super(InsnType.FILL_ARRAY, 1);
 		this.target = target;
-		this.arrayData = new FillArrayData(); // or set to a non-null default value
 		addArg(arg);
 	}
 
