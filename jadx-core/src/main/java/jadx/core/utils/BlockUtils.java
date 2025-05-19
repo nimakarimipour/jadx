c50d913cfd4aaea9664f47f451a533d68d03bd92
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 20:35:08 -0700

    fix: 61 - DEREFERENCE_NULLABLE - if (!bitSet.isEmpty()) { - dereferenced expression bitSet is @Nullable

--- jadx-core/src/main/java/jadx/core/utils/BlockUtils.java
+++ jadx-core/src/main/java/jadx/core/utils/BlockUtils.java
@@ -359,7 +359,10 @@ public class BlockUtils {
 		return new BitSet(mth.getBasicBlocks().size());
 	}
 
-	public static BitSet copyBlocksBitSet(MethodNode mth, @Nullable BitSet bitSet) {
+	public static BitSet copyBlocksBitSet(MethodNode mth, BitSet bitSet) {
+		if (bitSet == null) {
+			return new BitSet(mth.getBasicBlocks().size());
+		}
 		BitSet copy = new BitSet(mth.getBasicBlocks().size());
 		if (!bitSet.isEmpty()) {
 			copy.or(bitSet);
