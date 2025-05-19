5bc7fde8814cbf7176c5eca3f3a19c793c08ef4c
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 20:32:58 -0700

    Revert "fix: 33 - RETURN_NULLABLE - return info; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 4cbcff4f436cc0c55b19664f51d89410ab3af043.

--- jadx-core/src/main/java/jadx/core/dex/visitors/regions/IfMakerHelper.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/regions/IfMakerHelper.java
@@ -48,13 +48,10 @@ public class IfMakerHelper {
 		return info;
 	}
 
-	static IfInfo searchNestedIf(IfInfo info) {
+	static IfInfo searchNestedIf(@Nullable IfInfo info) {
 		IfInfo next = mergeNestedIfNodes(info);
 		if (next != null) {
 			return next;
-		}
-		if (info == null) {
-			return new IfInfo(); // or throw an exception, or handle as needed
 		}
 		return info;
 	}
