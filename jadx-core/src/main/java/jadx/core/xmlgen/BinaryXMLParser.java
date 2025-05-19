f670d85aca818dc446ac519dcc95b8035b87d793
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 20:31:41 -0700

    fix: 18 - PASS_NULLABLE - str = deobfClassName(str); - passing @Nullable parameter 'str' where @NonNull is required

--- jadx-core/src/main/java/jadx/core/xmlgen/BinaryXMLParser.java
+++ jadx-core/src/main/java/jadx/core/xmlgen/BinaryXMLParser.java
@@ -391,9 +391,8 @@ public class BinaryXMLParser extends CommonBinaryParser {
 	}
 
 	private void decodeAttribute(int attributeNS, int attrValDataType, int attrValData,
-			@Nullable String shortNsName, String attrName) {
+			String shortNsName, String attrName) {
 		if (attrValDataType == TYPE_REFERENCE) {
-			// reference custom processing
 			String resName = resNames.get(attrValData);
 			if (resName != null) {
 				writer.add('@');
@@ -414,7 +413,7 @@ public class BinaryXMLParser extends CommonBinaryParser {
 		} else {
 			String str = valuesParser.decodeValue(attrValDataType, attrValData);
 			memorizePackageName(attrName, str);
-			if (isDeobfCandidateAttr(shortNsName, attrName)) {
+			if (str != null && isDeobfCandidateAttr(shortNsName, attrName)) {
 				str = deobfClassName(str);
 			}
 			attachClassNode(writer, attrName, str);
