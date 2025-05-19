2e8a0bf948e6427c46e684891580ba5f83a9dd78
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 20:40:24 -0700

    fix: 116 - RETURN_NULLABLE - return null; - returning @Nullable expression from method with @NonNull return type

--- jadx-core/src/main/java/jadx/core/clsp/ClsSet.java
+++ jadx-core/src/main/java/jadx/core/clsp/ClsSet.java
@@ -417,7 +417,7 @@ public class ClsSet {
 	private ArgType readArgType(DataInputStream in) throws IOException {
 		int ordinal = in.readByte();
 		if (ordinal == -1) {
-			return null;
+			throw new JadxRuntimeException("Unexpected null-like ordinal value encountered");
 		}
 		if (ordinal >= TypeEnum.values().length) {
 			throw new JadxRuntimeException("Incorrect ordinal for type enum: " + ordinal);
