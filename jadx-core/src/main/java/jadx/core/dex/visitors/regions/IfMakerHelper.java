1d1683a113fcc0816bc4c271ab16b3adf22be04c
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 20:31:51 -0700

    Revert "fix: 19 - DEREFERENCE_NULLABLE - BlockNode curThen = currentIf.getThenBlock(); - dereferenced expression currentIf is @Nullable"
    This reverts commit ede3eedbc1a2462896d7016d9f19c22efc666f89.

--- jadx-core/src/main/java/jadx/core/dex/visitors/regions/IfMakerHelper.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/regions/IfMakerHelper.java
@@ -126,10 +126,8 @@ public class IfMakerHelper {
 		return true;
 	}
 
-	static IfInfo mergeNestedIfNodes(IfInfo currentIf) {
-		if (currentIf == null) {
-			return null;
-		}
+	@Nullable
+	static IfInfo mergeNestedIfNodes(@Nullable IfInfo currentIf) {
 		BlockNode curThen = currentIf.getThenBlock();
 		BlockNode curElse = currentIf.getElseBlock();
 		if (curThen == curElse) {
@@ -156,17 +154,20 @@ public class IfMakerHelper {
 		if (assignInlineNeeded) {
 			for (BlockNode mergedBlock : currentIf.getMergedBlocks()) {
 				if (mergedBlock.contains(AFlag.LOOP_START)) {
+					// don't inline assigns into loop condition
 					return currentIf;
 				}
 			}
 		}
 
 		if (isInversionNeeded(currentIf, nextIf)) {
+			// invert current node for match pattern
 			nextIf = IfInfo.invert(nextIf);
 		}
 		boolean thenPathSame = isEqualPaths(curThen, nextIf.getThenBlock());
 		boolean elsePathSame = isEqualPaths(curElse, nextIf.getElseBlock());
 		if (!thenPathSame && !elsePathSame) {
+			// complex condition, run additional checks
 			if (checkConditionBranches(curThen, curElse)
 					|| checkConditionBranches(curElse, curThen)) {
 				return null;
@@ -177,6 +178,8 @@ public class IfMakerHelper {
 				return checkForTernaryInCondition(currentIf);
 			}
 
+			// this is nested conditions with different mode (i.e (a && b) || c),
+			// search next condition for merge, get null if failed
 			IfInfo tmpIf = mergeNestedIfNodes(nextIf);
 			if (tmpIf != null) {
 				nextIf = tmpIf;
@@ -193,6 +196,7 @@ public class IfMakerHelper {
 			if (assignInlineNeeded) {
 				boolean sameOuts = (thenPathSame && !followThenBranch) || (elsePathSame && followThenBranch);
 				if (!sameOuts) {
+					// don't inline assigns inside simple condition
 					currentIf.resetForceInlineInsns();
 					return currentIf;
 				}
@@ -200,6 +204,7 @@ public class IfMakerHelper {
 		}
 
 		IfInfo result = mergeIfInfo(currentIf, nextIf, followThenBranch);
+		// search next nested if block
 		return searchNestedIf(result);
 	}
 
