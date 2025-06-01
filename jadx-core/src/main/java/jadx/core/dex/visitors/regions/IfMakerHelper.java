5bd88428964efdc789effcc7028f8fe515c00179
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 16:09:54 -0700

    Revert "fix: 96 - DEREFERENCE_NULLABLE - BlockNode thenBlock = info.getThenBlock(); - dereferenced expression info is @Nullable"
    This reverts commit 593f916a47019b3f922186342acde1f75f8af71a.

--- jadx-core/src/main/java/jadx/core/dex/visitors/regions/IfMakerHelper.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/regions/IfMakerHelper.java
@@ -57,11 +57,7 @@ public class IfMakerHelper {
 	}
 
 	@Nullable
-	static IfInfo restructureIf(MethodNode mth, BlockNode block, IfInfo info) {
-		if (info == null) {
-			return null;
-		}
-
+	static IfInfo restructureIf(MethodNode mth, BlockNode block, @Nullable IfInfo info) {
 		BlockNode thenBlock = info.getThenBlock();
 		BlockNode elseBlock = info.getElseBlock();
 
@@ -71,6 +67,7 @@ public class IfMakerHelper {
 			return ifInfo;
 		}
 
+		// select 'then', 'else' and 'exit' blocks
 		if (thenBlock.contains(AFlag.RETURN) && elseBlock.contains(AFlag.RETURN)) {
 			info.setOutBlock(null);
 			return info;
