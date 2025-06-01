766641923cf02edcb26b4732cd7770434080e4ef
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 15:39:45 -0700

    fix: 5 - FIELD_NO_INIT - private RootNode root; - @NonNull field root not initialized

--- jadx-core/src/main/java/jadx/core/dex/visitors/MethodInvokeVisitor.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/MethodInvokeVisitor.java
@@ -7,6 +7,8 @@ import java.util.function.Function;
 
 import javax.annotation.Nullable;
 
+import com.uber.nullaway.annotations.Initializer;
+
 import jadx.api.ICodeWriter;
 import jadx.core.Consts;
 import jadx.core.dex.attributes.AFlag;
@@ -47,6 +49,7 @@ import jadx.core.utils.exceptions.JadxRuntimeException;
 public class MethodInvokeVisitor extends AbstractVisitor {
 	private RootNode root;
 
+	@Initializer
 	@Override
 	public void init(RootNode root) {
 		this.root = root;
