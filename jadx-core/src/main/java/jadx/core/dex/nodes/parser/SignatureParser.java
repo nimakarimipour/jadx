8ccf1c60c9d6ecf5250d447d41c5a7cc4e175113
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 15:46:44 -0700

    Revert "fix: 20 - RETURN_NULLABLE - return null; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 0045288abc43dcae9b706e95e8d86ebdcff567b5.

--- jadx-core/src/main/java/jadx/core/dex/nodes/parser/SignatureParser.java
+++ jadx-core/src/main/java/jadx/core/dex/nodes/parser/SignatureParser.java
@@ -130,7 +130,6 @@ public class SignatureParser {
 		return skipUntil(lastChar) ? inclusiveSlice() : null;
 	}
 
-	@SuppressWarnings("NullAway")
 	public ArgType consumeType() {
 		char ch = next();
 		switch (ch) {
