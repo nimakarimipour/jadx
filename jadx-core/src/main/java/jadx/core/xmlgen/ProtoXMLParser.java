0f4e28a782a0ecaa6ebb09ec76ed107899a1566a
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 15:49:28 -0700

    fix: 37 - ASSIGN_FIELD_NULLABLE - nsMap = null; - assigning @Nullable expression to @NonNull field

--- jadx-core/src/main/java/jadx/core/xmlgen/ProtoXMLParser.java
+++ jadx-core/src/main/java/jadx/core/xmlgen/ProtoXMLParser.java
@@ -20,6 +20,7 @@ import jadx.core.dex.nodes.RootNode;
 import jadx.core.utils.StringUtils;
 
 public class ProtoXMLParser {
+	@Nullable
 	private Map<String, String> nsMap;
 	private final Map<String, String> tagAttrDeobfNames = new HashMap<>();
 
@@ -83,7 +84,7 @@ public class ProtoXMLParser {
 	private void decode(XmlAttribute a) {
 		writer.add(' ');
 		String namespace = a.getNamespaceUri();
-		if (!namespace.isEmpty()) {
+		if (!namespace.isEmpty() && nsMap.containsKey(namespace)) {
 			writer.add(nsMap.get(namespace)).add(':');
 		}
 		String name = a.getName();
@@ -93,10 +94,12 @@ public class ProtoXMLParser {
 	}
 
 	private void decode(XmlNamespace n) {
-		String prefix = n.getPrefix();
-		String uri = n.getUri();
-		nsMap.put(uri, prefix);
-		writer.add(" xmlns:").add(prefix).add("=\"").add(uri).add('"');
+		if (nsMap != null) {
+			String prefix = n.getPrefix();
+			String uri = n.getUri();
+			nsMap.put(uri, prefix);
+			writer.add(" xmlns:").add(prefix).add("=\"").add(uri).add('"');
+		}
 	}
 
 	private void memorizePackageName(String attrName, String attrValue) {
