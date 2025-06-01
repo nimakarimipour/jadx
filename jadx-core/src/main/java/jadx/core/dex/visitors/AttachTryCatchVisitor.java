7d2db72c66d8d26863452ad7aacbe56279ce71d9
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 15:51:17 -0700

    Revert "fix: 55 - DEREFERENCE_NULLABLE - insn.add(AFlag.TRY_LEAVE); - dereferenced expression insn is @Nullable"
    This reverts commit 577d8b87fa99c7b44e93136c3104f11cbcd9637c.

--- jadx-core/src/main/java/jadx/core/dex/visitors/AttachTryCatchVisitor.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/AttachTryCatchVisitor.java
@@ -86,8 +86,9 @@ public class AttachTryCatchVisitor extends AbstractVisitor {
 			}
 		}
 		if (tryBlockStarted) {
-			NullabilityUtil.castToNonnull(insn, "checked by loop assignment").add(AFlag.TRY_LEAVE);
+			insn.add(AFlag.TRY_LEAVE);
 		} else {
+			// no instructions found in range -> add nop at start offset
 			InsnNode nop = insertNOP(insnByOffset, aTry.getStartOffset());
 			nop.add(AFlag.TRY_ENTER);
 			nop.add(AFlag.TRY_LEAVE);
