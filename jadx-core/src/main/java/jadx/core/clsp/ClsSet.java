54ab0002cc7f2fce250502d8d529a1b51b8d1a75
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 15:39:43 -0700

    Revert "fix: 4 - METHOD_NO_INIT - public ClsSet(RootNode root) { - initializer method does not guarantee @NonNull field classes (line 75) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 957bfcc643adbb75a62d6873cb48acbd24d3b3dc.

--- jadx-core/src/main/java/jadx/core/clsp/ClsSet.java
+++ jadx-core/src/main/java/jadx/core/clsp/ClsSet.java
@@ -72,7 +72,6 @@ public class ClsSet {
 		PRIMITIVE
 	}
 
-	@Nullable
 	private ClspClass[] classes;
 
 	public void loadFromClstFile() throws IOException, DecodeException {
@@ -83,7 +82,7 @@ public class ClsSet {
 			}
 			load(input);
 		}
-		if (classes != null && LOG.isDebugEnabled()) {
+		if (LOG.isDebugEnabled()) {
 			long time = System.currentTimeMillis() - startTime;
 			int methodsCount = Stream.of(classes).mapToInt(clspClass -> clspClass.getMethodsMap().size()).sum();
 			LOG.debug("Clst file loaded in {}ms, classes: {}, methods: {}", time, classes.length, methodsCount);
@@ -211,9 +210,6 @@ public class ClsSet {
 	}
 
 	private void save(OutputStream output) throws IOException {
-		if (classes == null) {
-			throw new IllegalStateException("Classes array is not initialized");
-		}
 		DataOutputStream out = new DataOutputStream(output);
 		out.writeBytes(JADX_CLS_SET_HEADER);
 		out.writeByte(VERSION);
@@ -426,9 +422,6 @@ public class ClsSet {
 		if (ordinal >= TypeEnum.values().length) {
 			throw new JadxRuntimeException("Incorrect ordinal for type enum: " + ordinal);
 		}
-		if (classes == null) {
-			throw new JadxRuntimeException("Classes array is not initialized");
-		}
 		switch (TypeEnum.values()[ordinal]) {
 			case WILDCARD:
 				ArgType.WildcardBound bound = ArgType.WildcardBound.getByNum(in.readByte());
@@ -508,16 +501,10 @@ public class ClsSet {
 	}
 
 	public int getClassesCount() {
-		if (classes == null) {
-			throw new IllegalStateException("Classes array is not initialized");
-		}
 		return classes.length;
 	}
 
 	public void addToMap(Map<String, ClspClass> nameMap) {
-		if (classes == null) {
-			throw new IllegalStateException("Classes not loaded");
-		}
 		for (ClspClass cls : classes) {
 			nameMap.put(cls.getName(), cls);
 		}
