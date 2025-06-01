0d8ef51360cca175a53f90daca5361f0948f8be1
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 16:09:50 -0700

    Revert "fix: 95 - RETURN_NULLABLE - return null; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 083c6a518383216428ae0baedc513a5200b9edd7.

--- jadx-core/src/main/java/jadx/core/codegen/ClassGen.java
+++ jadx-core/src/main/java/jadx/core/codegen/ClassGen.java
@@ -545,9 +545,6 @@ public class ClassGen {
 
 	private void addInnerType(ICodeWriter code, ArgType baseType) {
 		ArgType innerType = baseType.getInnerType();
-		if (innerType == null) {
-			return; // Or handle according to your logic
-		}
 		ArgType outerType = innerType.getOuterType();
 		if (outerType != null) {
 			useClassWithShortName(code, baseType, outerType);
