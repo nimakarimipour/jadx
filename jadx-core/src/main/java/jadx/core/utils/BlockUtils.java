23922111f4babb4c8536dc7820dbb761f11e3fd0
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 14:38:24 -0700

    fix: 110 - DEREFERENCE_NULLABLE - d.set(block.getId()); - dereferenced expression d is @Nullable

--- jadx-core/src/main/java/jadx/core/utils/BlockUtils.java
+++ jadx-core/src/main/java/jadx/core/utils/BlockUtils.java
@@ -19,6 +19,8 @@ import java.util.function.Predicate;
 
 import org.jetbrains.annotations.Nullable;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
+
 import jadx.core.dex.attributes.AFlag;
 import jadx.core.dex.attributes.AType;
 import jadx.core.dex.attributes.nodes.LoopInfo;
@@ -1183,7 +1185,7 @@ public class BlockUtils {
 						d.and(scPDoms);
 					}
 				}
-				d.set(block.getId());
+				Nullability.castToNonnull(d, "populated in map").set(block.getId());
 				if (!changed && !d.equals(domSet)) {
 					changed = true;
 					map.put(block, d);
