75c539b4c366696adb01bdfbfe613044ad4efa9c
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:36:12 -0700

    fix: 23 - FIELD_NO_INIT - private StringBuilder curVariable; - @NonNull field TemplateFile$ParserState.curVariable not initialized

--- jadx-core/src/main/java/jadx/core/export/TemplateFile.java
+++ jadx-core/src/main/java/jadx/core/export/TemplateFile.java
@@ -28,7 +28,7 @@ public class TemplateFile {
 
 	private static class ParserState {
 		private State state = State.NONE;
-		private StringBuilder curVariable;
+		private StringBuilder curVariable = new StringBuilder();
 		private boolean skip;
 	}
 
