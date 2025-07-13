fa8f3d53821e83aba3f08fb464f4eed69a86e009
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 13:13:45 -0700

    fix: 55 - DEREFERENCE_NULLABLE - insn.add(AFlag.TRY_LEAVE); - dereferenced expression insn is @Nullable

--- jadx-core/src/main/java/jadx/core/dex/visitors/AttachTryCatchVisitor.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/AttachTryCatchVisitor.java
@@ -8,6 +8,8 @@ import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
+
 import jadx.api.plugins.input.data.ICatch;
 import jadx.api.plugins.input.data.ITry;
 import jadx.api.plugins.utils.Utils;
@@ -86,9 +88,8 @@ public class AttachTryCatchVisitor extends AbstractVisitor {
 			}
 		}
 		if (tryBlockStarted) {
-			insn.add(AFlag.TRY_LEAVE);
+			Nullability.castToNonnull(insn, "set when flag true").add(AFlag.TRY_LEAVE);
 		} else {
-			// no instructions found in range -> add nop at start offset
 			InsnNode nop = insertNOP(insnByOffset, aTry.getStartOffset());
 			nop.add(AFlag.TRY_ENTER);
 			nop.add(AFlag.TRY_LEAVE);
