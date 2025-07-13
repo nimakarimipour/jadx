188f2d3474ff5e8dfe08446605ef8938605066cb
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 13:20:25 -0700

    fix: 71 - DEREFERENCE_NULLABLE - addEdgeInsn(edge.getSource(), edge.getTarget(), insn); - dereferenced expression edge is @Nullable

--- jadx-core/src/main/java/jadx/core/dex/attributes/nodes/EdgeInsnAttr.java
+++ jadx-core/src/main/java/jadx/core/dex/attributes/nodes/EdgeInsnAttr.java
@@ -4,6 +4,8 @@ import java.util.Objects;
 
 import javax.annotation.Nullable;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
+
 import jadx.api.plugins.input.data.attributes.IJadxAttribute;
 import jadx.core.dex.attributes.AType;
 import jadx.core.dex.attributes.AttrList;
@@ -18,7 +20,9 @@ public class EdgeInsnAttr implements IJadxAttribute {
 	private final InsnNode insn;
 
 	public static void addEdgeInsn(@Nullable Edge edge, InsnNode insn) {
-		addEdgeInsn(edge.getSource(), edge.getTarget(), insn);
+		addEdgeInsn(Nullability.castToNonnull(edge).getSource(),
+				Nullability.castToNonnull(edge).getTarget(),
+				insn);
 	}
 
 	public static void addEdgeInsn(BlockNode start, BlockNode end, InsnNode insn) {
