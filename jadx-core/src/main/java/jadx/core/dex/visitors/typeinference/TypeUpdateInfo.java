1738cac886b1589e8d94fe3578a560e24acc2a41
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:38:21 -0700

    fix: 39 - DEREFERENCE_NULLABLE - return arg.getType(); - dereferenced expression arg is @Nullable

--- jadx-core/src/main/java/jadx/core/dex/visitors/typeinference/TypeUpdateInfo.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/typeinference/TypeUpdateInfo.java
@@ -46,6 +46,9 @@ public class TypeUpdateInfo {
 	}
 
 	public ArgType getType(@Nullable InsnArg arg) {
+		if (arg == null) {
+			return null; // or throw an exception based on what makes sense for your use case
+		}
 		for (TypeUpdateEntry update : updates) {
 			if (update.getArg() == arg) {
 				return update.getType();
