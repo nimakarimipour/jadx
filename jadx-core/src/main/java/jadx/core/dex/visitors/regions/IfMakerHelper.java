40880ec5d4064579d4dee5c8352e856fea9d4bf7
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 08:01:24 -0700

    before:105to104 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression currentIf is @Nullable' /home/nima/Developer/nullness-benchmarks/jadx/jadx-core/src/main/java/jadx/core/dex/visitors/regions/IfMakerHelper.java:131 		BlockNode curThen = currentIf.getThenBlock();

--- jadx-core/src/main/java/jadx/core/dex/visitors/regions/IfMakerHelper.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/regions/IfMakerHelper.java
@@ -126,87 +126,82 @@ public class IfMakerHelper {
 		return true;
 	}
 
-	@Nullable
-	static IfInfo mergeNestedIfNodes(@Nullable IfInfo currentIf) {
-		BlockNode curThen = currentIf.getThenBlock();
-		BlockNode curElse = currentIf.getElseBlock();
-		if (curThen == curElse) {
-			return null;
-		}
-		if (BlockUtils.isFollowBackEdge(curThen)
-				|| BlockUtils.isFollowBackEdge(curElse)) {
-			return null;
-		}
-		boolean followThenBranch;
-		IfInfo nextIf = getNextIf(currentIf, curThen);
-		if (nextIf != null) {
-			followThenBranch = true;
-		} else {
-			nextIf = getNextIf(currentIf, curElse);
-			if (nextIf != null) {
-				followThenBranch = false;
-			} else {
-				return null;
-			}
-		}
-
-		boolean assignInlineNeeded = !nextIf.getForceInlineInsns().isEmpty();
-		if (assignInlineNeeded) {
-			for (BlockNode mergedBlock : currentIf.getMergedBlocks()) {
-				if (mergedBlock.contains(AFlag.LOOP_START)) {
-					// don't inline assigns into loop condition
-					return currentIf;
-				}
-			}
-		}
-
-		if (isInversionNeeded(currentIf, nextIf)) {
-			// invert current node for match pattern
-			nextIf = IfInfo.invert(nextIf);
-		}
-		boolean thenPathSame = isEqualPaths(curThen, nextIf.getThenBlock());
-		boolean elsePathSame = isEqualPaths(curElse, nextIf.getElseBlock());
-		if (!thenPathSame && !elsePathSame) {
-			// complex condition, run additional checks
-			if (checkConditionBranches(curThen, curElse)
-					|| checkConditionBranches(curElse, curThen)) {
-				return null;
-			}
-			BlockNode otherBranchBlock = followThenBranch ? curElse : curThen;
-			otherBranchBlock = BlockUtils.followEmptyPath(otherBranchBlock);
-			if (!isPathExists(nextIf.getFirstIfBlock(), otherBranchBlock)) {
-				return checkForTernaryInCondition(currentIf);
-			}
-
-			// this is nested conditions with different mode (i.e (a && b) || c),
-			// search next condition for merge, get null if failed
-			IfInfo tmpIf = mergeNestedIfNodes(nextIf);
-			if (tmpIf != null) {
-				nextIf = tmpIf;
-				if (isInversionNeeded(currentIf, nextIf)) {
-					nextIf = IfInfo.invert(nextIf);
-				}
-				if (!canMerge(currentIf, nextIf, followThenBranch)) {
-					return currentIf;
-				}
-			} else {
-				return currentIf;
-			}
-		} else {
-			if (assignInlineNeeded) {
-				boolean sameOuts = (thenPathSame && !followThenBranch) || (elsePathSame && followThenBranch);
-				if (!sameOuts) {
-					// don't inline assigns inside simple condition
-					currentIf.resetForceInlineInsns();
-					return currentIf;
-				}
-			}
-		}
-
-		IfInfo result = mergeIfInfo(currentIf, nextIf, followThenBranch);
-		// search next nested if block
-		return searchNestedIf(result);
-	}
+	@Nullable static IfInfo mergeNestedIfNodes(@Nullable IfInfo currentIf) {
+       if (currentIf == null) {
+           return null;
+       }
+       BlockNode curThen = currentIf.getThenBlock();
+       BlockNode curElse = currentIf.getElseBlock();
+       if (curThen == curElse) {
+           return null;
+       }
+       if (BlockUtils.isFollowBackEdge(curThen)
+               || BlockUtils.isFollowBackEdge(curElse)) {
+           return null;
+       }
+       boolean followThenBranch;
+       IfInfo nextIf = getNextIf(currentIf, curThen);
+       if (nextIf != null) {
+           followThenBranch = true;
+       } else {
+           nextIf = getNextIf(currentIf, curElse);
+           if (nextIf != null) {
+               followThenBranch = false;
+           } else {
+               return null;
+           }
+       }
+ 
+       boolean assignInlineNeeded = !nextIf.getForceInlineInsns().isEmpty();
+       if (assignInlineNeeded) {
+           for (BlockNode mergedBlock : currentIf.getMergedBlocks()) {
+               if (mergedBlock.contains(AFlag.LOOP_START)) {
+                   return currentIf;
+               }
+           }
+       }
+ 
+       if (isInversionNeeded(currentIf, nextIf)) {
+           nextIf = IfInfo.invert(nextIf);
+       }
+       boolean thenPathSame = isEqualPaths(curThen, nextIf.getThenBlock());
+       boolean elsePathSame = isEqualPaths(curElse, nextIf.getElseBlock());
+       if (!thenPathSame && !elsePathSame) {
+           if (checkConditionBranches(curThen, curElse)
+                   || checkConditionBranches(curElse, curThen)) {
+               return null;
+           }
+           BlockNode otherBranchBlock = followThenBranch ? curElse : curThen;
+           otherBranchBlock = BlockUtils.followEmptyPath(otherBranchBlock);
+           if (!isPathExists(nextIf.getFirstIfBlock(), otherBranchBlock)) {
+               return checkForTernaryInCondition(currentIf);
+           }
+ 
+           IfInfo tmpIf = mergeNestedIfNodes(nextIf);
+           if (tmpIf != null) {
+               nextIf = tmpIf;
+               if (isInversionNeeded(currentIf, nextIf)) {
+                   nextIf = IfInfo.invert(nextIf);
+               }
+               if (!canMerge(currentIf, nextIf, followThenBranch)) {
+                   return currentIf;
+               }
+           } else {
+               return currentIf;
+           }
+       } else {
+           if (assignInlineNeeded) {
+               boolean sameOuts = (thenPathSame && !followThenBranch) || (elsePathSame && followThenBranch);
+               if (!sameOuts) {
+                   currentIf.resetForceInlineInsns();
+                   return currentIf;
+               }
+           }
+       }
+ 
+       IfInfo result = mergeIfInfo(currentIf, nextIf, followThenBranch);
+       return searchNestedIf(result);
+   }
 
 	@Nullable
 	private static IfInfo checkForTernaryInCondition(IfInfo currentIf) {
