3a58202d3dd5ec141f6dee504f473d8a77ad4700
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 13:09:37 -0700

    fix: 46 - RETURN_NULLABLE - return storage.get(type); - returning @Nullable expression from method with @NonNull return type

--- jadx-core/src/main/java/jadx/core/codegen/InsnGen.java
+++ jadx-core/src/main/java/jadx/core/codegen/InsnGen.java
@@ -9,6 +9,8 @@ import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
+
 import jadx.api.CommentsLevel;
 import jadx.api.ICodeWriter;
 import jadx.api.metadata.annotations.InsnCodeOffset;
@@ -753,37 +755,46 @@ public class InsnGen {
 			throw new CodegenException("Anonymous inner class unlimited recursion detected."
 					+ " Convert class to inner: " + cls.getClassInfo().getFullName());
 		}
-		ArgType parent = cls.get(AType.ANONYMOUS_CLASS).getBaseType();
-		// hide empty anonymous constructors
-		for (MethodNode ctor : cls.getMethods()) {
-			if (ctor.contains(AFlag.ANONYMOUS_CONSTRUCTOR)
-					&& RegionUtils.isEmpty(ctor.getRegion())) {
-				ctor.add(AFlag.DONT_GENERATE);
-			}
-		}
-		code.attachDefinition(cls);
-		code.add("new ");
-		useClass(code, parent);
-		MethodNode callMth = mth.root().resolveMethod(insn.getCallMth());
-		if (callMth != null) {
-			// copy var names
-			List<RegisterArg> mthArgs = callMth.getArgRegs();
-			int argsCount = Math.min(insn.getArgsCount(), mthArgs.size());
-			for (int i = 0; i < argsCount; i++) {
-				InsnArg arg = insn.getArg(i);
-				if (arg.isRegister()) {
-					RegisterArg mthArg = mthArgs.get(i);
-					RegisterArg insnArg = (RegisterArg) arg;
-					mthArg.getSVar().setCodeVar(insnArg.getSVar().getCodeVar());
+
+		AnonymousClassAttr anonClassAttr = cls.get(AType.ANONYMOUS_CLASS);
+		if (anonClassAttr != null) {
+			ArgType parent = anonClassAttr.getBaseType();
+
+			// hide empty anonymous constructors
+			for (MethodNode ctor : cls.getMethods()) {
+				if (ctor.contains(AFlag.ANONYMOUS_CONSTRUCTOR)
+						&& RegionUtils.isEmpty(ctor.getRegion())) {
+					ctor.add(AFlag.DONT_GENERATE);
 				}
 			}
-		}
-		generateMethodArguments(code, insn, 0, callMth);
-		code.add(' ');
 
-		ClassGen classGen = new ClassGen(cls, mgen.getClassGen().getParentGen());
-		classGen.setOuterNameGen(mgen.getNameGen());
-		classGen.addClassBody(code, true);
+			code.attachDefinition(cls);
+			code.add("new ");
+			useClass(code, parent);
+			MethodNode callMth = mth.root().resolveMethod(insn.getCallMth());
+			if (callMth != null) {
+				// copy var names
+				List<RegisterArg> mthArgs = callMth.getArgRegs();
+				int argsCount = Math.min(insn.getArgsCount(), mthArgs.size());
+				for (int i = 0; i < argsCount; i++) {
+					InsnArg arg = insn.getArg(i);
+					if (arg.isRegister()) {
+						RegisterArg mthArg = mthArgs.get(i);
+						RegisterArg insnArg = (RegisterArg) arg;
+						mthArg.getSVar().setCodeVar(insnArg.getSVar().getCodeVar());
+					}
+				}
+			}
+			generateMethodArguments(code, insn, 0, callMth);
+			code.add(' ');
+
+			ClassGen classGen = new ClassGen(cls, mgen.getClassGen().getParentGen());
+			classGen.setOuterNameGen(mgen.getNameGen());
+			classGen.addClassBody(code, true);
+		} else {
+			// Handle case where AType.ANONYMOUS_CLASS is null if necessary
+			throw new CodegenException("Anonymous class attribute missing for class: " + cls.getClassInfo().getFullName());
+		}
 	}
 
 	private void makeInvoke(InvokeNode insn, ICodeWriter code) throws CodegenException {
@@ -932,12 +943,12 @@ public class InsnGen {
 	}
 
 	private void makeInlinedLambdaMethod(ICodeWriter code, InvokeCustomNode customNode, MethodNode callMth) throws CodegenException {
-		MethodGen callMthGen = new MethodGen(mgen.getClassGen(), callMth);
+		MethodGen callMthGen = new MethodGen(mgen.getClassGen(), Nullability.castToNonnull(callMth));
 		NameGen nameGen = callMthGen.getNameGen();
 		nameGen.inheritUsedNames(this.mgen.getNameGen());
 
 		List<ArgType> implArgs = customNode.getImplMthInfo().getArgumentsTypes();
-		List<RegisterArg> callArgs = callMth.getArgRegs();
+		List<RegisterArg> callArgs = Nullability.castToNonnull(callMth).getArgRegs();
 		if (implArgs.isEmpty()) {
 			code.add("()");
 		} else {
@@ -951,9 +962,8 @@ public class InsnGen {
 				defVar(code, argCodeVar);
 			}
 		}
-		// force set external arg names into call method args
 		int extArgsCount = customNode.getArgsCount();
-		int startArg = customNode.getHandleType() == MethodHandleType.INVOKE_STATIC ? 0 : 1; // skip 'this' arg
+		int startArg = customNode.getHandleType() == MethodHandleType.INVOKE_STATIC ? 0 : 1;
 		for (int i = startArg; i < extArgsCount; i++) {
 			RegisterArg extArg = (RegisterArg) customNode.getArg(i);
 			RegisterArg callRegArg = callArgs.get(i);
