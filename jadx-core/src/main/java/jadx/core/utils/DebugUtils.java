29965da69afdba9e5a57945aeb5664fe7ecd180c
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:53:37 -0700

    Revert "fix: 8 - FIELD_NO_INIT - private static Map<String, Long> execTimes; - @NonNull static field execTimes not initialized"
    This reverts commit 2d07d1edcd9d07c9996ae0506a8daca25956ef51.

--- jadx-core/src/main/java/jadx/core/utils/DebugUtils.java
+++ jadx-core/src/main/java/jadx/core/utils/DebugUtils.java
@@ -13,12 +13,8 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import javax.annotation.Nullable;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import edu.ucr.cs.riple.annotator.util.Nullability;
 
 import jadx.api.ICodeWriter;
 import jadx.api.impl.SimpleCodeWriter;
@@ -251,7 +247,6 @@ public class DebugUtils {
 		return t -> seen.add(keyExtractor.apply(t));
 	}
 
-	@Nullable
 	private static Map<String, Long> execTimes;
 
 	public static void initExecTimes() {
@@ -263,27 +258,17 @@ public class DebugUtils {
 	}
 
 	public static void mergeExecTime(String tag, long execTimeMillis) {
-		if (execTimes == null) {
-			execTimes = new ConcurrentHashMap<>();
-		}
 		execTimes.merge(tag, execTimeMillis, Long::sum);
 	}
 
 	public static void printExecTimes() {
-		if (execTimes != null) {
-			System.out.println("Exec times:");
-			execTimes.forEach((tag, time) -> System.out.println(" " + tag + ": " + time + "ms"));
-		} else {
-			System.out.println("Exec times not initialized.");
-		}
+		System.out.println("Exec times:");
+		execTimes.forEach((tag, time) -> System.out.println(" " + tag + ": " + time + "ms"));
 	}
 
 	public static void printExecTimesWithTotal(long totalMillis) {
-		if (execTimes == null) {
-			initExecTimes();
-		}
 		System.out.println("Exec times: total " + totalMillis + "ms");
-		Nullability.castToNonnull(execTimes, "initialized if null").forEach((tag, time) -> System.out.println(" " + tag + ": " + time + "ms"
+		execTimes.forEach((tag, time) -> System.out.println(" " + tag + ": " + time + "ms"
 				+ String.format(" (%.2f%%)", time * 100. / (double) totalMillis)));
 	}
 }
