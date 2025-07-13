c01956eec12ac2792f75f5c74cc451a7409783c5
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:36:29 -0700

    Revert "fix: 25 - DEREFERENCE_NULLABLE - if (!kotlinVarNameSourceMethods.contains(invokeMth)) { - dereferenced expression kotlinVarNameSourceMethods is @Nullable"
    This reverts commit 7f2bbeb92dcc1f8b4654b3752aaf60c8f7e65783.

--- jadx-core/src/main/java/jadx/core/dex/visitors/kotlin/ProcessKotlinInternals.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/kotlin/ProcessKotlinInternals.java
@@ -109,7 +109,7 @@ public class ProcessKotlinInternals extends AbstractVisitor {
 			return;
 		}
 		MethodInfo invokeMth = ((InvokeNode) insn).getCallMth();
-		if (kotlinVarNameSourceMethods == null || !kotlinVarNameSourceMethods.contains(invokeMth)) {
+		if (!kotlinVarNameSourceMethods.contains(invokeMth)) {
 			return;
 		}
 		InsnArg firstArg = insn.getArg(0);
@@ -124,6 +124,7 @@ public class ProcessKotlinInternals extends AbstractVisitor {
 				renamed = checkAndRename(varArg, str);
 			}
 		} else if (argsCount == 3) {
+			// TODO: use second arg for rename class
 			String str = getConstString(mth, insn, 2);
 			if (str != null) {
 				renamed = checkAndRename(varArg, str);
