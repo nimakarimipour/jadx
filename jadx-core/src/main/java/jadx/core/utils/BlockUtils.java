ced0b1f5822854b858f1a624ed5063d3605ae0de
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 14:37:36 -0700

    fix: 109 - DEREFERENCE_NULLABLE - postDoms.clear(block.getId()); - dereferenced expression postDoms is @Nullable

--- jadx-core/src/main/java/jadx/core/utils/BlockUtils.java
+++ jadx-core/src/main/java/jadx/core/utils/BlockUtils.java
@@ -19,6 +19,8 @@ import java.util.function.Predicate;
 
 import org.jetbrains.annotations.Nullable;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
+
 import jadx.core.dex.attributes.AFlag;
 import jadx.core.dex.attributes.AType;
 import jadx.core.dex.attributes.nodes.LoopInfo;
@@ -1193,7 +1195,7 @@ public class BlockUtils {
 
 		blockNodes.forEach(block -> {
 			BitSet postDoms = map.get(block);
-			postDoms.clear(block.getId());
+			Nullability.castToNonnull(postDoms, "initialized in loop").clear(block.getId());
 			if (postDoms.isEmpty()) {
 				map.put(block, EmptyBitSet.EMPTY);
 			}
