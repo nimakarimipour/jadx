2c113abef550e1264cf2097cc26829b72cb6ada1
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 08:04:23 -0700

    before:91to90 - Type='RETURN_NULLABLE', message='returning @Nullable expression from method with @NonNull return type' /home/nima/Developer/nullness-benchmarks/jadx/jadx-core/src/main/java/jadx/core/xmlgen/ManifestAttributes.java:67 		return instance;

--- jadx-core/src/main/java/jadx/core/xmlgen/ManifestAttributes.java
+++ jadx-core/src/main/java/jadx/core/xmlgen/ManifestAttributes.java
@@ -57,15 +57,16 @@ public class ManifestAttributes {
 	private static ManifestAttributes instance;
 
 	public static ManifestAttributes getInstance() {
-		if (instance == null) {
-			try {
-				instance = new ManifestAttributes();
-			} catch (Exception e) {
-				LOG.error("Failed to create ManifestAttributes", e);
-			}
-		}
-		return instance;
-	}
+       if (instance == null) {
+           try {
+               instance = new ManifestAttributes();
+           } catch (Exception e) {
+               LOG.error("Failed to create ManifestAttributes", e);
+               instance = new ManifestAttributes(); // Create a new instance to ensure non-null return
+           }
+       }
+       return instance;
+   }
 
 	private ManifestAttributes() {
 		parseAll();
