3202832518d09c793aa1ee3fe010978e3d2a4986
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:45:41 -0700

    fix: 89 - DEREFERENCE_NULLABLE - FieldFixType fieldFixType = fieldFixInfo.fieldFixMap.get(fieldInfo); - dereferenced expression fieldFixInfo.fieldFixMap is @Nullable

--- jadx-core/src/main/java/jadx/core/dex/visitors/ShadowFieldVisitor.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/ShadowFieldVisitor.java
@@ -144,7 +144,7 @@ public class ShadowFieldVisitor extends AbstractVisitor {
 			return;
 		}
 		FieldFixInfo fieldFixInfo = fixInfoMap.get(type.getObject());
-		if (fieldFixInfo == null) {
+		if (fieldFixInfo == null || fieldFixInfo.fieldFixMap == null) {
 			return;
 		}
 		FieldFixType fieldFixType = fieldFixInfo.fieldFixMap.get(fieldInfo);
