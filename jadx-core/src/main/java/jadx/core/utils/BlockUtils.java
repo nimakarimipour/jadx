73f685ab6d7ad06b83f7ab80278bdc496b005c1b
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 13:30:37 -0700

    fix: 77 - DEREFERENCE_NULLABLE - List<BlockNode> successors = block.getSuccessors(); - dereferenced expression block is @Nullable

--- jadx-core/src/main/java/jadx/core/utils/BlockUtils.java
+++ jadx-core/src/main/java/jadx/core/utils/BlockUtils.java
@@ -19,6 +19,8 @@ import java.util.function.Predicate;
 
 import org.jetbrains.annotations.Nullable;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
+
 import jadx.core.dex.attributes.AFlag;
 import jadx.core.dex.attributes.AType;
 import jadx.core.dex.attributes.nodes.LoopInfo;
@@ -226,7 +228,7 @@ public class BlockUtils {
 	}
 
 	public static boolean isExitBlock(@Nullable BlockNode block) {
-		List<BlockNode> successors = block.getSuccessors();
+		List<BlockNode> successors = Nullability.castToNonnull(block).getSuccessors();
 		if (successors.isEmpty()) {
 			return true;
 		}
