5085880762445063a2b6f80a70425eb1f7556ff8
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:33:16 -0700

    Revert "fix: 2 - DEREFERENCE_NULLABLE - Mode mode = cond.getMode(); - dereferenced expression cond is @Nullable"
    This reverts commit dce65358da210db8420285da144178a9b4c6fa0b.

--- jadx-core/src/main/java/jadx/core/dex/regions/conditions/IfCondition.java
+++ jadx-core/src/main/java/jadx/core/dex/regions/conditions/IfCondition.java
@@ -119,9 +119,6 @@ public final class IfCondition extends AttrNode {
 	}
 
 	public static IfCondition invert(@Nullable IfCondition cond) {
-		if (cond == null) {
-			throw new NullPointerException("IfCondition 'cond' cannot be null");
-		}
 		Mode mode = cond.getMode();
 		switch (mode) {
 			case COMPARE:
