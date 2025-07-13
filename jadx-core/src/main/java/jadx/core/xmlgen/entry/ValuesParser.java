b7dab44eddbe12f2b44d1d35110ac04a0b8a21ef
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:34:58 -0700

    Revert "fix: 14 - FIELD_NO_INIT - private static Map<Integer, String> androidResMap; - @NonNull static field androidResMap not initialized"
    This reverts commit 778e20fcd3b313e9aed2228fdbcc2ecc1b71e5f5.

--- jadx-core/src/main/java/jadx/core/xmlgen/entry/ValuesParser.java
+++ jadx-core/src/main/java/jadx/core/xmlgen/entry/ValuesParser.java
@@ -17,7 +17,7 @@ import jadx.core.xmlgen.XmlGenUtils;
 public class ValuesParser extends ParserConstants {
 	private static final Logger LOG = LoggerFactory.getLogger(ValuesParser.class);
 
-	private static Map<Integer, String> androidResMap = loadAndroidResMap();
+	private static Map<Integer, String> androidResMap;
 
 	@Nullable
 	private final String[] strings;
