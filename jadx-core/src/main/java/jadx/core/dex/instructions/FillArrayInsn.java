415085704301d0ce101def18d5fb9206928d5f0c
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 14:05:32 -0700

    Revert "fix: 93 - METHOD_NO_INIT - public FillArrayInsn(InsnArg arg, int target) { - initializer method does not guarantee @NonNull field arrayData (line 13) is initialized along all control-flow paths (remember to check for exceptions or early returns)."
    This reverts commit 6d530170eb3a290e56ca748ac2a4c9f82d42013e.

--- jadx-core/src/main/java/jadx/core/dex/instructions/FillArrayInsn.java
+++ jadx-core/src/main/java/jadx/core/dex/instructions/FillArrayInsn.java
@@ -3,10 +3,6 @@ package jadx.core.dex.instructions;
 import java.util.List;
 import java.util.Objects;
 
-import javax.annotation.Nullable;
-
-import edu.ucr.cs.riple.annotator.util.Nullability;
-
 import jadx.core.dex.instructions.args.ArgType;
 import jadx.core.dex.instructions.args.InsnArg;
 import jadx.core.dex.instructions.args.LiteralArg;
@@ -14,7 +10,6 @@ import jadx.core.dex.nodes.InsnNode;
 
 public final class FillArrayInsn extends InsnNode {
 	private final int target;
-	@Nullable
 	private FillArrayData arrayData;
 
 	public FillArrayInsn(InsnArg arg, int target) {
@@ -55,15 +50,15 @@ public final class FillArrayInsn extends InsnNode {
 	}
 
 	public int getSize() {
-		return arrayData != null ? Nullability.castToNonnull(arrayData, "not null check").getSize() : 0;
+		return arrayData.getSize();
 	}
 
 	public ArgType getElementType() {
-		return arrayData != null ? arrayData.getElementType() : null;
+		return arrayData.getElementType();
 	}
 
 	public List<LiteralArg> getLiteralArgs(ArgType elType) {
-		return Objects.requireNonNull(arrayData, "arrayData is null").getLiteralArgs(elType);
+		return arrayData.getLiteralArgs(elType);
 	}
 
 	public String dataToString() {
