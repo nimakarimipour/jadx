202d3d9f22b95a94c7e205196c0ffb2f9e29a2c0
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:40:29 -0700

    Revert "fix: 54 - RETURN_NULLABLE - return result; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 6e3893536500ebca4a99f3ef7ba4718ddd97a0ae.

--- jadx-core/src/main/java/jadx/core/deobf/Deobfuscator.java
+++ jadx-core/src/main/java/jadx/core/deobf/Deobfuscator.java
@@ -303,7 +303,7 @@ public class Deobfuscator {
 			}
 		} while (!fullPkgName.isEmpty() && result != null);
 
-		return result != null ? result : rootPackage;
+		return result;
 	}
 
 	String getNameWithoutPackage(ClassInfo clsInfo) {
