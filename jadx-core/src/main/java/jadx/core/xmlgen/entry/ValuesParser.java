4f15e9eae7ab75370839f5c2f766bd851d36a201
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:48:32 -0700

    fix: 107 - DEREFERENCE_NULLABLE - return strings[data]; - dereferenced expression strings is @Nullable

--- jadx-core/src/main/java/jadx/core/xmlgen/entry/ValuesParser.java
+++ jadx-core/src/main/java/jadx/core/xmlgen/entry/ValuesParser.java
@@ -106,7 +106,11 @@ public class ValuesParser extends ParserConstants {
 			case TYPE_NULL:
 				return null;
 			case TYPE_STRING:
-				return strings[data];
+				if (strings != null && data < strings.length) {
+					return strings[data];
+				} else {
+					return "?unknown_string";
+				}
 			case TYPE_INT_DEC:
 				return Integer.toString(data);
 			case TYPE_INT_HEX:
