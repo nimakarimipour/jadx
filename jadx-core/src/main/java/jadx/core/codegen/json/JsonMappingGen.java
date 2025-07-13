0c96542f7e9696358cc2467eba1d1a9f91d4755a
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 01:36:50 -0700

    before:70to69 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression jsonCls.getFields() is @Nullable' /home/nima/Developer/nullness-benchmarks/jadx/jadx-core/src/main/java/jadx/core/codegen/json/JsonMappingGen.java:101 			jsonCls.getFields().add(jsonField);

--- jadx-core/src/main/java/jadx/core/codegen/json/JsonMappingGen.java
+++ jadx-core/src/main/java/jadx/core/codegen/json/JsonMappingGen.java
@@ -90,18 +90,18 @@ public class JsonMappingGen {
  }
 
 	private static void addFields(ClassNode cls, JsonClsMapping jsonCls) {
-		List<FieldNode> fields = cls.getFields();
-		if (fields.isEmpty()) {
-			return;
-		}
-		jsonCls.setFields(new ArrayList<>(fields.size()));
-		for (FieldNode field : fields) {
-			JsonFieldMapping jsonField = new JsonFieldMapping();
-			jsonField.setName(field.getName());
-			jsonField.setAlias(field.getAlias());
-			jsonCls.getFields().add(jsonField);
-		}
-	}
+ 		List<FieldNode> fields = cls.getFields();
+ 		if (fields.isEmpty()) {
+ 			return;
+ 		}
+ 		jsonCls.setFields(new ArrayList<>(fields.size()));
+ 		for (FieldNode field : fields) {
+ 			JsonFieldMapping jsonField = new JsonFieldMapping();
+ 			jsonField.setName(field.getName());
+ 			jsonField.setAlias(field.getAlias());
+ 			Nullability.castToNonnull(jsonCls.getFields(), "ensuring initialization").add(jsonField);
+ 		}
+ }
 
 	private JsonMappingGen() {
 	}
