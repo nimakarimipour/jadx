b1e5ae3cadc881987c377aa817c8e676820c545d
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:42:24 -0700

    Revert "fix: 67 - DEREFERENCE_NULLABLE - if (valueOfMths.contains(callMth)) { - dereferenced expression valueOfMths is @Nullable"
    This reverts commit 3db4f50f226fb7494c39a58e8ab9da42d1833aab.

--- jadx-core/src/main/java/jadx/core/dex/visitors/DeboxingVisitor.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/DeboxingVisitor.java
@@ -92,7 +92,7 @@ public class DeboxingVisitor extends AbstractVisitor {
 			return null;
 		}
 		MethodInfo callMth = insnNode.getCallMth();
-		if (valueOfMths != null && valueOfMths.contains(callMth)) {
+		if (valueOfMths.contains(callMth)) {
 			RegisterArg resArg = insnNode.getResult();
 			InsnArg arg = insnNode.getArg(0);
 			if (arg.isLiteral()) {
