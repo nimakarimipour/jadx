4b71a43f8cc078d60a5c7d3b10341af2f01bcdd1
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:40:56 -0700

    Revert "fix: 57 - DEREFERENCE_NULLABLE - int len = str.length(); - dereferenced expression str is @Nullable"
    This reverts commit 6871ce2e912ed02fb2a99f2110db40b453a170a3.

--- jadx-core/src/main/java/jadx/core/deobf/NameMapper.java
+++ jadx-core/src/main/java/jadx/core/deobf/NameMapper.java
@@ -131,14 +131,11 @@ public class NameMapper {
 	}
 
 	public static boolean isAllCharsPrintable(@Nullable String str) {
-		if (str == null) {
-			return false;
-		}
 		int len = str.length();
 		int offset = 0;
 		while (offset < len) {
 			int codePoint = str.codePointAt(offset);
-			if (!isPrintableCodePoint(codePoint)) {
+			if (!isPrintableAsciiCodePoint(codePoint)) {
 				return false;
 			}
 			offset += Character.charCount(codePoint);
