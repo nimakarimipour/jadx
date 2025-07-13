9dca8bbf75587b9ffaadfa060897c8ec3c926d83
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 13:10:07 -0700

    Revert "fix: 48 - DEREFERENCE_NULLABLE - return inputFilePath.getParent().resolve(baseName + ".jobf"); - dereferenced expression inputFilePath.getParent() is @Nullable"
    This reverts commit 5a5a809631704f738f80eb16f1fe3c092a33917a.

--- jadx-core/src/main/java/jadx/core/deobf/DeobfPresets.java
+++ jadx-core/src/main/java/jadx/core/deobf/DeobfPresets.java
@@ -17,8 +17,6 @@ import javax.annotation.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
-
 import jadx.api.JadxArgs;
 import jadx.api.args.DeobfuscationMapFileMode;
 import jadx.core.dex.info.ClassInfo;
@@ -57,7 +55,7 @@ public class DeobfPresets {
 		}
 		Path inputFilePath = jadxArgs.getInputFiles().get(0).toPath().toAbsolutePath();
 		String baseName = FileUtils.getPathBaseName(inputFilePath);
-		return Nullability.castToNonnull(inputFilePath.getParent()).resolve(baseName + ".jobf");
+		return inputFilePath.getParent().resolve(baseName + ".jobf");
 	}
 
 	private DeobfPresets(Path deobfMapFile) {
