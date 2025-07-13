6aa8f1a14d97f371d9b77925a0ddfc70190e7df6
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 01:37:07 -0700

    before:69to68 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression parentClass is @Nullable' /home/nima/Developer/nullness-benchmarks/jadx/jadx-core/src/main/java/jadx/core/dex/info/ClassInfo.java:90 			return parentClass.getAliasPkg();

--- jadx-core/src/main/java/jadx/core/dex/info/ClassInfo.java
+++ jadx-core/src/main/java/jadx/core/dex/info/ClassInfo.java
@@ -18,8 +18,8 @@ public final class ClassInfo implements Comparable<ClassInfo> {
 	@Nullable("for inner classes")
 	private String pkg;
 	private String fullName;
-	@Nullable
-	private ClassInfo parentClass;
+	
+	@Nullable private ClassInfo parentClass;
 	@Nullable
 	private ClassAliasInfo alias;
 
@@ -87,11 +87,11 @@ public final class ClassInfo implements Comparable<ClassInfo> {
 	}
 
 	public String getAliasPkg() {
-		if (isInner()) {
-			return parentClass.getAliasPkg();
-		}
-		return alias == null ? getPackage() : alias.getPkg();
-	}
+       if (isInner() && parentClass != null) {
+           return parentClass.getAliasPkg();
+       }
+       return alias == null ? getPackage() : alias.getPkg();
+   }
 
 	public String getAliasShortName() {
 		return alias == null ? getShortName() : alias.getShortName();
