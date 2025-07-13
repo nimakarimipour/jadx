2a9214c739ff9836e6f650e1642241088710a6e0
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:41:48 -0700

    fix: 63 - RETURN_NULLABLE - return clsp; - returning @Nullable expression from method with @NonNull return type

--- jadx-core/src/main/java/jadx/core/dex/nodes/RootNode.java
+++ jadx-core/src/main/java/jadx/core/dex/nodes/RootNode.java
@@ -494,6 +494,9 @@ public class RootNode {
 	}
 
 	public ClspGraph getClsp() {
+		if (clsp == null) {
+			throw new IllegalStateException("ClspGraph has not been initialized");
+		}
 		return clsp;
 	}
 
