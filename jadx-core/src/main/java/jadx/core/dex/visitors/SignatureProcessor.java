c5865bcecc14a37e2922d3ed3e29e847f8893405
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:33:25 -0700

    Revert "fix: 3 - FIELD_NO_INIT - private RootNode root; - @NonNull field root not initialized"
    This reverts commit e6592960acff2e461b72e2f22f604791deff6231.

--- jadx-core/src/main/java/jadx/core/dex/visitors/SignatureProcessor.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/SignatureProcessor.java
@@ -21,12 +21,7 @@ import jadx.core.utils.exceptions.JadxException;
 
 public class SignatureProcessor extends AbstractVisitor {
 
-	private final RootNode root;
-
-	@Override
-	public void init(RootNode root) {
-		this.root = root;
-	}
+	private RootNode root;
 
 	@Override
 	public void init(RootNode root) {
