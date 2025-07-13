e4f884557b715c0bee2412d544bf12f311fea373
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 13:01:36 -0700

    fix: 30 - DEREFERENCE_NULLABLE - return pkg.isEmpty() ? shortName : pkg + '.' + shortName; - dereferenced expression pkg is @Nullable

--- jadx-core/src/main/java/jadx/core/dex/info/ClassInfo.java
+++ jadx-core/src/main/java/jadx/core/dex/info/ClassInfo.java
@@ -6,6 +6,8 @@ import java.util.Objects;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
+
 import jadx.core.dex.instructions.args.ArgType;
 import jadx.core.dex.nodes.ClassNode;
 import jadx.core.dex.nodes.RootNode;
@@ -170,7 +172,7 @@ public final class ClassInfo implements Comparable<ClassInfo> {
 			}
 			return parentFullName + innerSep + shortName;
 		}
-		return pkg.isEmpty() ? shortName : pkg + '.' + shortName;
+		return Nullability.castToNonnull(pkg).isEmpty() ? shortName : Nullability.castToNonnull(pkg) + '.' + shortName;
 	}
 
 	private String makeFullName() {
