56df0344a8e31387170ebf2d80771fe52ccb00aa
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 14:04:51 -0700

    fix: 92 - DEREFERENCE_NULLABLE - makeDirs(path.toAbsolutePath().getParent().toFile()); - dereferenced expression path.toAbsolutePath().getParent() is @Nullable

--- jadx-core/src/main/java/jadx/core/utils/files/FileUtils.java
+++ jadx-core/src/main/java/jadx/core/utils/files/FileUtils.java
@@ -32,6 +32,8 @@ import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
+
 import jadx.core.utils.exceptions.JadxRuntimeException;
 
 public class FileUtils {
@@ -79,7 +81,7 @@ public class FileUtils {
 
 	public static void makeDirsForFile(Path path) {
 		if (path != null) {
-			makeDirs(path.toAbsolutePath().getParent().toFile());
+			makeDirs(Nullability.castToNonnull(path.toAbsolutePath().getParent()).toFile());
 		}
 	}
 
