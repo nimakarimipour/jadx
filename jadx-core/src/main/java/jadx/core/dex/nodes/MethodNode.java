98d5890088fd2386d4a083ada3fea5859a4133b7
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:34:40 -0700

    fix: 12 - ASSIGN_FIELD_NULLABLE - blocks = null; - assigning @Nullable expression to @NonNull field

--- jadx-core/src/main/java/jadx/core/dex/nodes/MethodNode.java
+++ jadx-core/src/main/java/jadx/core/dex/nodes/MethodNode.java
@@ -113,7 +113,7 @@ public class MethodNode extends NotificationAttrNode implements IMethodDetails, 
 		argsList = null;
 		sVars = Collections.emptyList();
 		instructions = null;
-		blocks = null;
+		blocks = Collections.emptyList(); // Set to an empty list instead of null
 		enterBlock = null;
 		exitBlock = null;
 		region = null;
