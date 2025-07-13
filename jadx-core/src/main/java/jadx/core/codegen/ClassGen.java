474dcd9f75710327e0ebc7f3b64c6df9489fa1dd
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 13:03:27 -0700

    Revert "fix: 36 - DEREFERENCE_NULLABLE - String fullName = extClsInfo.getAliasFullName(); - dereferenced expression extClsInfo is @Nullable"
    This reverts commit 22770b00166eb44b0237b458b63a9f71f73cbcef.

--- jadx-core/src/main/java/jadx/core/codegen/ClassGen.java
+++ jadx-core/src/main/java/jadx/core/codegen/ClassGen.java
@@ -15,8 +15,6 @@ import java.util.stream.Stream;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
-
 import jadx.api.CommentsLevel;
 import jadx.api.ICodeInfo;
 import jadx.api.ICodeWriter;
@@ -618,7 +616,7 @@ public class ClassGen {
 	}
 
 	private String useClassInternal(ClassInfo useCls, @Nullable ClassInfo extClsInfo) {
-		String fullName = Nullability.castToNonnull(extClsInfo.getAliasFullName());
+		String fullName = extClsInfo.getAliasFullName();
 		if (fallback || !useImports) {
 			return fullName;
 		}
