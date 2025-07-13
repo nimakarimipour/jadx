cc8d0002c44559e5dc9966a253c3ffcf1e4602a5
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 13:10:28 -0700

    fix: 50 - DEREFERENCE_NULLABLE - jsonCls.getMethods().add(jsonMethod); - dereferenced expression jsonCls.getMethods() is @Nullable

--- jadx-core/src/main/java/jadx/core/codegen/json/JsonMappingGen.java
+++ jadx-core/src/main/java/jadx/core/codegen/json/JsonMappingGen.java
@@ -13,6 +13,8 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
+
 import jadx.api.JadxArgs;
 import jadx.core.codegen.json.mapping.JsonClsMapping;
 import jadx.core.codegen.json.mapping.JsonFieldMapping;
@@ -84,7 +86,7 @@ public class JsonMappingGen {
 			jsonMethod.setName(methodInfo.getName());
 			jsonMethod.setAlias(methodInfo.getAlias());
 			jsonMethod.setOffset("0x" + Long.toHexString(method.getMethodCodeOffset()));
-			jsonCls.getMethods().add(jsonMethod);
+			Nullability.castToNonnull(jsonCls.getMethods(), "initialized before access").add(jsonMethod);
 		}
 	}
 
