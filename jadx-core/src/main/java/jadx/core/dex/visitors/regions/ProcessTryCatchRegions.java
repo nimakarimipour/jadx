3548beb320fd02c28a7efabf14fb2125018d0c8f
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 13:57:19 -0700

    Revert "fix: 86 - RETURN_NULLABLE - return topSplitter; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit df6a97281f4362d554604559a7914235795ea3b4.

--- jadx-core/src/main/java/jadx/core/dex/visitors/regions/ProcessTryCatchRegions.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/regions/ProcessTryCatchRegions.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
-
 import jadx.core.dex.attributes.AFlag;
 import jadx.core.dex.attributes.AType;
 import jadx.core.dex.nodes.BlockNode;
@@ -83,7 +81,7 @@ public class ProcessTryCatchRegions extends AbstractRegionVisitor {
 		Region tryRegion = new Region(replaceRegion);
 		List<IContainer> subBlocks = replaceRegion.getSubBlocks();
 		for (IContainer cont : subBlocks) {
-			if (RegionUtils.hasPathThroughBlock(Nullability.castToNonnull(dominator), cont)) {
+			if (RegionUtils.hasPathThroughBlock(dominator, cont)) {
 				if (isHandlerPath(tb, cont)) {
 					break;
 				}
