e39e3f73eba627911f30a52f4cbb4a6148bd76a5
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:37:27 -0700

    Revert "fix: 32 - DEREFERENCE_NULLABLE - InsnNode inlCopy = mia.getInsn().copyWithoutResult(); - dereferenced expression mia.getInsn() is @Nullable"
    This reverts commit d24422c39bc4374f35ffa7d1623bd00c9914adbe.

--- jadx-core/src/main/java/jadx/core/dex/visitors/InlineMethods.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/InlineMethods.java
@@ -78,23 +78,23 @@ public class InlineMethods extends AbstractVisitor {
 	}
 
 	private void inlineMethod(MethodNode mth, MethodNode callMth, MethodInlineAttr mia, BlockNode block, InvokeNode insn) {
-		if (mia == null || mia.getInsn() == null) {
-			throw new IllegalArgumentException("Method inline attribute or instruction is null");
-		}
 		InsnNode inlCopy = mia.getInsn().copyWithoutResult();
 		RegisterArg resultArg = insn.getResult();
 		if (resultArg != null) {
 			inlCopy.setResult(resultArg.duplicate());
 		} else if (isAssignNeeded(mia.getInsn(), insn, callMth)) {
+			// add fake result to make correct java expression (see test TestGetterInlineNegative)
 			inlCopy.setResult(makeFakeArg(mth, callMth.getReturnType(), "unused"));
 		}
 		if (!callMth.getMethodInfo().getArgumentsTypes().isEmpty()) {
+			// remap args
 			InsnArg[] regs = new InsnArg[callMth.getRegsCount()];
 			int[] regNums = mia.getArgsRegNums();
 			for (int i = 0; i < regNums.length; i++) {
 				InsnArg arg = insn.getArg(i);
 				regs[regNums[i]] = arg;
 			}
+			// replace args
 			List<RegisterArg> inlArgs = new ArrayList<>();
 			inlCopy.getRegisterArgs(inlArgs);
 			for (RegisterArg r : inlArgs) {
@@ -115,6 +115,7 @@ public class InlineMethods extends AbstractVisitor {
 		if (!BlockUtils.replaceInsn(mth, block, insn, inlCopy)) {
 			mth.addWarnComment("Failed to inline method: " + callMth);
 		}
+		// replaceInsn replaces the attributes as well, make sure to preserve METHOD_DETAILS
 		if (methodDetailsAttr != null) {
 			inlCopy.addAttr(methodDetailsAttr);
 		}
