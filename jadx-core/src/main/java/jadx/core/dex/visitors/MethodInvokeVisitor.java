2980a02d4f56915f215b862566d30ae442f0c655
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:52:53 -0700

    Revert "fix: 5 - FIELD_NO_INIT - private RootNode root; - @NonNull field root not initialized"
    This reverts commit 6d558b886901e86cec907418d806809f569d6dee.

--- jadx-core/src/main/java/jadx/core/dex/visitors/MethodInvokeVisitor.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/MethodInvokeVisitor.java
@@ -7,8 +7,6 @@ import java.util.function.Function;
 
 import javax.annotation.Nullable;
 
-import com.uber.nullaway.annotations.Initializer;
-
 import jadx.api.ICodeWriter;
 import jadx.core.Consts;
 import jadx.core.dex.attributes.AFlag;
@@ -47,10 +45,8 @@ import jadx.core.utils.exceptions.JadxRuntimeException;
 		}
 )
 public class MethodInvokeVisitor extends AbstractVisitor {
-	@SuppressWarnings("NullAway.Init")
 	private RootNode root;
 
-	@Initializer
 	@Override
 	public void init(RootNode root) {
 		this.root = root;
