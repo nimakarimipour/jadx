e5b9529f1ca642a0c9444362aee2cbe933637a75
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:36:20 -0700

    fix: 24 - ASSIGN_FIELD_NULLABLE - this.compare = null; - assigning @Nullable expression to @NonNull field

--- jadx-core/src/main/java/jadx/core/dex/regions/conditions/IfCondition.java
+++ jadx-core/src/main/java/jadx/core/dex/regions/conditions/IfCondition.java
@@ -47,7 +47,10 @@ public final class IfCondition extends AttrNode {
 	private IfCondition(Mode mode, List<IfCondition> args) {
 		this.mode = mode;
 		this.args = args;
-		this.compare = null;
+		if (mode == Mode.COMPARE) {
+			throw new IllegalArgumentException("Mode cannot be COMPARE when using this constructor");
+		}
+		this.compare = new Compare(); // Assuming a valid default Compare instance is possible
 	}
 
 	private IfCondition(IfCondition c) {
