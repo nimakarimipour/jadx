4157441b0d79f44f8a27a9875fbeccfc85ada2d9
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:37:09 -0700

    fix: 30 - FIELD_NO_INIT - protected ParserStream is; - @NonNull field is not initialized

--- jadx-core/src/main/java/jadx/core/xmlgen/CommonBinaryParser.java
+++ jadx-core/src/main/java/jadx/core/xmlgen/CommonBinaryParser.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class CommonBinaryParser extends ParserConstants {
-	protected ParserStream is;
+	protected ParserStream is = new ParserStream();
 
 	protected String[] parseStringPool() throws IOException {
 		is.checkInt16(RES_STRING_POOL_TYPE, "String pool expected");
