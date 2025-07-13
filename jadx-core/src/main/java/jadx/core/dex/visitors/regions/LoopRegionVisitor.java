d3e7e6b19012a40bf091a4dba3bc647800487afc
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:49:35 -0700

    Revert "fix: 113 - DEREFERENCE_NULLABLE - InsnArg iterableArg = assignInsn.getArg(0); - dereferenced expression assignInsn is @Nullable"
    This reverts commit a6f4bab5ff998d70fc7fba3b2ec1c63f82b9b1e8.

--- jadx-core/src/main/java/jadx/core/dex/visitors/regions/LoopRegionVisitor.java
+++ jadx-core/src/main/java/jadx/core/dex/visitors/regions/LoopRegionVisitor.java
@@ -258,9 +258,6 @@ public class LoopRegionVisitor extends AbstractVisitor implements IRegionVisitor
 		}
 		List<RegisterArg> itUseList = sVar.getUseList();
 		InsnNode assignInsn = iteratorArg.getAssignInsn();
-		if (assignInsn == null) { // Check if assignInsn is not null
-			return false;
-		}
 		if (itUseList.size() != 2) {
 			return false;
 		}
@@ -296,6 +293,7 @@ public class LoopRegionVisitor extends AbstractVisitor implements IRegionVisitor
 					if (castArg != null && castArg.getParentInsn() != null) {
 						castArg.getParentInsn().replaceArg(castArg, iterVar);
 					} else {
+						// cast not inlined
 						toSkip.add(parentInsn);
 					}
 				} else {
@@ -303,7 +301,7 @@ public class LoopRegionVisitor extends AbstractVisitor implements IRegionVisitor
 					if (iterVar == null) {
 						return false;
 					}
-					iterVar.remove(AFlag.REMOVE);
+					iterVar.remove(AFlag.REMOVE); // restore variable from inlined insn
 					nextCall.add(AFlag.DONT_GENERATE);
 					if (!fixIterableType(mth, iterableArg, iterVar)) {
 						return false;
