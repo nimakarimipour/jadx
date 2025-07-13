5b8702837f68c71e6c834d43508f0f882e22a1b7
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:58:50 -0700

    Revert "fix: 18 - PASS_NULLABLE - str = deobfClassName(str); - passing @Nullable parameter 'str' where @NonNull is required"
    This reverts commit 8fa0ef56df129e765caf6190f4749225f7a42628.

--- jadx-core/src/main/java/jadx/core/xmlgen/BinaryXMLParser.java
+++ jadx-core/src/main/java/jadx/core/xmlgen/BinaryXMLParser.java
@@ -13,8 +13,6 @@ import javax.annotation.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
-
 import jadx.api.ICodeInfo;
 import jadx.api.ICodeWriter;
 import jadx.api.ResourcesLoader;
@@ -417,7 +415,7 @@ public class BinaryXMLParser extends CommonBinaryParser {
 			String str = valuesParser.decodeValue(attrValDataType, attrValData);
 			memorizePackageName(attrName, str);
 			if (isDeobfCandidateAttr(shortNsName, attrName)) {
-				str = deobfClassName(Nullability.castToNonnull(str));
+				str = deobfClassName(str);
 			}
 			attachClassNode(writer, attrName, str);
 			writer.add(str != null ? StringUtils.escapeXML(str) : "null");
