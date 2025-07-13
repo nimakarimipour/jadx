b4a006ba29ef24d37c1323833ccf1479d13c367f
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:35:15 -0700

    Revert "fix: 16 - RETURN_NULLABLE - return body; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 8f6d2be5b2257857ae355f82f6da7fa27c19bdea.

--- jadx-core/src/main/java/jadx/core/dex/regions/loops/LoopRegion.java
+++ jadx-core/src/main/java/jadx/core/dex/regions/loops/LoopRegion.java
@@ -56,9 +56,6 @@ public final class LoopRegion extends ConditionRegion {
 	}
 
 	public IRegion getBody() {
-		if (body == null) {
-			throw new NullPointerException("The body cannot be null");
-		}
 		return body;
 	}
 
