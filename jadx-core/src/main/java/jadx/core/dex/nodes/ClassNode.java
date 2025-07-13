b680e903f6f039fd14f37b3a4a2c8918bd9cc345
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:53:25 -0700

    fix: 7 - DEREFERENCE_NULLABLE - if (!superClass.equals(ArgType.OBJECT)) { - dereferenced expression superClass is @Nullable

--- jadx-core/src/main/java/jadx/core/dex/nodes/ClassNode.java
+++ jadx-core/src/main/java/jadx/core/dex/nodes/ClassNode.java
@@ -63,6 +63,7 @@ public class ClassNode extends NotificationAttrNode implements ILoadable, ICodeN
 
 	private final ClassInfo clsInfo;
 	private AccessInfo accessFlags;
+
 	@Nullable
 	private ArgType superClass;
 	private List<ArgType> interfaces;
@@ -597,7 +598,7 @@ public class ClassNode extends NotificationAttrNode implements ILoadable, ICodeN
 	public void visitSuperTypes(BiConsumer<ArgType, ArgType> consumer) {
 		TypeUtils typeUtils = root.getTypeUtils();
 		ArgType thisType = this.getType();
-		if (!superClass.equals(ArgType.OBJECT)) {
+		if (superClass != null && !superClass.equals(ArgType.OBJECT)) {
 			consumer.accept(thisType, superClass);
 			typeUtils.visitSuperTypes(superClass, consumer);
 		}
