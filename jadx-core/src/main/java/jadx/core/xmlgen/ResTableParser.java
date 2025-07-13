3ed594e65148845e4a82c8788da4216e0c4d3d9d
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 08:10:04 -0700

    before:61to60 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression pkg.getKeyStrings() is @Nullable' /home/nima/Developer/nullness-benchmarks/jadx/jadx-core/src/main/java/jadx/core/xmlgen/ResTableParser.java:304 		String origKeyName = pkg.getKeyStrings()[key];

--- jadx-core/src/main/java/jadx/core/xmlgen/ResTableParser.java
+++ jadx-core/src/main/java/jadx/core/xmlgen/ResTableParser.java
@@ -292,44 +292,51 @@ public class ResTableParser extends CommonBinaryParser implements IResParser {
 	}
 
 	private void parseEntry(PackageChunk pkg, int typeId, int entryId, String config) throws IOException {
-		int size = is.readInt16();
-		int flags = is.readInt16();
-		int key = is.readInt32();
-		if (key == -1) {
-			return;
-		}
-
-		int resRef = pkg.getId() << 24 | typeId << 16 | entryId;
-		String typeName = pkg.getTypeStrings()[typeId - 1];
-		String origKeyName = pkg.getKeyStrings()[key];
-		ResourceEntry newResEntry = new ResourceEntry(resRef, pkg.getName(), typeName, getResName(typeName, resRef, origKeyName), config);
-		ResourceEntry prevResEntry = resStorage.searchEntryWithSameName(newResEntry);
-		if (prevResEntry != null) {
-			newResEntry = newResEntry.copyWithId();
-
-			// rename also previous entry for consistency
-			ResourceEntry replaceForPrevEntry = prevResEntry.copyWithId();
-			resStorage.replace(prevResEntry, replaceForPrevEntry);
-			resStorage.addRename(replaceForPrevEntry);
-		}
-		if (!Objects.equals(origKeyName, newResEntry.getKeyName())) {
-			resStorage.addRename(newResEntry);
-		}
-
-		if ((flags & FLAG_COMPLEX) != 0 || size == 16) {
-			int parentRef = is.readInt32();
-			int count = is.readInt32();
-			newResEntry.setParentRef(parentRef);
-			List<RawNamedValue> values = new ArrayList<>(count);
-			for (int i = 0; i < count; i++) {
-				values.add(parseValueMap());
-			}
-			newResEntry.setNamedValues(values);
-		} else {
-			newResEntry.setSimpleValue(parseValue());
-		}
-		resStorage.add(newResEntry);
-	}
+       int size = is.readInt16();
+       int flags = is.readInt16();
+       int key = is.readInt32();
+       if (key == -1) {
+           return;
+       }
+ 
+       int resRef = pkg.getId() << 24 | typeId << 16 | entryId;
+       String typeName = pkg.getTypeStrings()[typeId - 1];
+       String origKeyName = null;
+       String[] keyStrings = pkg.getKeyStrings();
+       if (keyStrings != null) {
+           origKeyName = keyStrings[key];
+       }
+       if (origKeyName == null) {
+           return; // or handle the error appropriately
+       }
+       ResourceEntry newResEntry = new ResourceEntry(resRef, pkg.getName(), typeName, getResName(typeName, resRef, origKeyName), config);
+       ResourceEntry prevResEntry = resStorage.searchEntryWithSameName(newResEntry);
+       if (prevResEntry != null) {
+           newResEntry = newResEntry.copyWithId();
+ 
+           // rename also previous entry for consistency
+           ResourceEntry replaceForPrevEntry = prevResEntry.copyWithId();
+           resStorage.replace(prevResEntry, replaceForPrevEntry);
+           resStorage.addRename(replaceForPrevEntry);
+       }
+       if (!Objects.equals(origKeyName, newResEntry.getKeyName())) {
+           resStorage.addRename(newResEntry);
+       }
+ 
+       if ((flags & FLAG_COMPLEX) != 0 || size == 16) {
+           int parentRef = is.readInt32();
+           int count = is.readInt32();
+           newResEntry.setParentRef(parentRef);
+           List<RawNamedValue> values = new ArrayList<>(count);
+           for (int i = 0; i < count; i++) {
+               values.add(parseValueMap());
+           }
+           newResEntry.setNamedValues(values);
+       } else {
+           newResEntry.setSimpleValue(parseValue());
+       }
+       resStorage.add(newResEntry);
+   }
 
 	private String getResName(String typeName, int resRef, String origKeyName) {
 		if (this.useRawResName) {
