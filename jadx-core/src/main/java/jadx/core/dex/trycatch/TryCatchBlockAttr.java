5fea65bc0b409a215170e938a4b3652990521c77
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:45:07 -0700

    Revert "fix: 85 - RETURN_NULLABLE - return topSplitter; - returning @Nullable expression from method with @NonNull return type"
    This reverts commit 7c08da0cffc16973679b0d114eadba7c857bdf59.

--- jadx-core/src/main/java/jadx/core/dex/trycatch/TryCatchBlockAttr.java
+++ jadx-core/src/main/java/jadx/core/dex/trycatch/TryCatchBlockAttr.java
@@ -120,9 +120,6 @@ public class TryCatchBlockAttr implements IJadxAttribute {
 	}
 
 	public BlockNode getTopSplitter() {
-		if (topSplitter == null) {
-			throw new IllegalStateException("topSplitter is null");
-		}
 		return topSplitter;
 	}
 
