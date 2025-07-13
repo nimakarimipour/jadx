51623d51258758f78225685d31d6235a88b26f57
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 13:15:36 -0700

    fix: 60 - DEREFERENCE_NULLABLE - if (inner.isInner()) { - dereferenced expression inner is @Nullable

--- jadx-core/src/main/java/jadx/core/codegen/ClassGen.java
+++ jadx-core/src/main/java/jadx/core/codegen/ClassGen.java
@@ -15,6 +15,8 @@ import java.util.stream.Stream;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
+
 import jadx.api.CommentsLevel;
 import jadx.api.ICodeInfo;
 import jadx.api.ICodeWriter;
@@ -714,7 +716,7 @@ public class ClassGen {
 	}
 
 	private static boolean isClassInnerFor(@Nullable ClassInfo inner, ClassInfo parent) {
-		if (inner.isInner()) {
+		if (Nullability.castToNonnull(inner).isInner()) {
 			ClassInfo p = inner.getParentClass();
 			return Objects.equals(p, parent) || isClassInnerFor(p, parent);
 		}
