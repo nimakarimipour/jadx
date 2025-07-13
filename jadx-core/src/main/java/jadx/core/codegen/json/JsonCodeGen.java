38696cd47206c331009d030ea71b988654cb3104
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 13:08:38 -0700

    Revert "fix: 44 - DEREFERENCE_NULLABLE - jsonCls.getFields().add(jsonField); - dereferenced expression jsonCls.getFields() is @Nullable"
    This reverts commit 65959849cd6fdd7aba1e8e6164776066d5bf2cf2.

--- jadx-core/src/main/java/jadx/core/codegen/json/JsonCodeGen.java
+++ jadx-core/src/main/java/jadx/core/codegen/json/JsonCodeGen.java
@@ -12,8 +12,6 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
-
 import jadx.api.ICodeInfo;
 import jadx.api.ICodeWriter;
 import jadx.api.JadxArgs;
@@ -134,7 +132,7 @@ public class JsonCodeGen {
 			classGen.addField(cw, field);
 			jsonField.setDeclaration(cw.getCodeStr());
 			jsonField.setAccessFlags(field.getAccessFlags().rawValue());
-			Nullability.castToNonnull(jsonCls.getFields(), "initialized with ArrayList").add(jsonField);
+			jsonCls.getFields().add(jsonField);
 		}
 	}
 
