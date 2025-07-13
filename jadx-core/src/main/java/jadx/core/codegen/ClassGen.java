18071114dc082233b7e32429c3d5fab4741861f9
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:41:22 -0700

    Revert "fix: 60 - DEREFERENCE_NULLABLE - if (inner.isInner()) { - dereferenced expression inner is @Nullable"
    This reverts commit edb119c26a1846e232722d8662b90ded34b70de9.

--- jadx-core/src/main/java/jadx/core/codegen/ClassGen.java
+++ jadx-core/src/main/java/jadx/core/codegen/ClassGen.java
@@ -714,7 +714,7 @@ public class ClassGen {
 	}
 
 	private static boolean isClassInnerFor(@Nullable ClassInfo inner, ClassInfo parent) {
-		if (inner != null && inner.isInner()) {
+		if (inner.isInner()) {
 			ClassInfo p = inner.getParentClass();
 			return Objects.equals(p, parent) || isClassInnerFor(p, parent);
 		}
