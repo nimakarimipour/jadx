cfc04671a986bb7bfc3707e64847e448d0b56200
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:34:06 -0700

    fix: 8 - FIELD_NO_INIT - private static Map<String, Long> execTimes; - @NonNull static field execTimes not initialized

--- jadx-core/src/main/java/jadx/core/utils/DebugUtils.java
+++ jadx-core/src/main/java/jadx/core/utils/DebugUtils.java
@@ -247,7 +247,7 @@ public class DebugUtils {
 		return t -> seen.add(keyExtractor.apply(t));
 	}
 
-	private static Map<String, Long> execTimes;
+	private static Map<String, Long> execTimes = new ConcurrentHashMap<>();
 
 	public static void initExecTimes() {
 		execTimes = new ConcurrentHashMap<>();
